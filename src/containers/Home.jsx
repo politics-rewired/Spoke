f032524d5bee78a5f10c6ab468607d0ce2443b1e
Benjamin Chrobot <benjamin.blair.chrobot@gmail.com>
2019-01-27 12:34:23 -0500

    Source Spoke logo locally. (#1)
    Source Spoke logo from alternate location

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -104,7 +104,7 @@ class Home extends React.Component {
       <div className={css(styles.container)}>
         <div className={css(styles.logoDiv)}>
           <img
-            src='https://s3-us-west-1.amazonaws.com/spoke-public/spoke_logo.svg'
+            src='https://politics-rewired.surge.sh/spoke_logo.svg'
             className={css(styles.logoImg)}
           />
         </div>
