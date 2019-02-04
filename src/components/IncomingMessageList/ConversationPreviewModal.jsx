cd950b0549674aaf76630a837339b746dc418b06
Benjamin Chrobot <benjamin.chrobot@alum.mit.edu>
2019-02-04 17:18:40 -0500

    Fix hard-coded contact ID.

--- src/components/IncomingMessageList/ConversationPreviewModal.jsx
+++ src/components/IncomingMessageList/ConversationPreviewModal.jsx
@@ -42,6 +42,7 @@ class ConversationPreviewBody extends Component {
   }
 
   render() {
+    const contactId = this.props.conversation.contact.id
     return (
       <div className={css(styles.container)}>
         <div className={css(styles.column)}>
@@ -52,9 +53,7 @@ class ConversationPreviewBody extends Component {
           />
         </div>
         <div className={css(styles.column)}>
-          <SurveyColumn
-            contactId={'1'}
-          />
+          <SurveyColumn contactId={contactId} />
         </div>
       </div>
     )
