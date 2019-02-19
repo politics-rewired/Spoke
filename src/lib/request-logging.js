5038145ff8ddbf5f0c7f10d2615dbbf617d7ea11
ben-pr-p <ben.paul.ryan.packer@gmail.com>
2019-02-18 22:55:55 -0500

    wip

---
+++ src/lib/request-logging.js
@@ -0,0 +1,24 @@
+import winston from 'winston'
+import expressWinston from 'express-winston'
+import winstonMongodb from 'winston-mongodb'
+
+expressWinston.requestWhitelist.push('body')
+
+export default expressWinston.logger({
+  transports: [
+    process.env.LOGGING_MONGODB_URI 
+    ? new winston.transports.MongoDB({
+        db: process.env.LOGGING_MONGODB_URI
+      })
+    : new winston.transports.Console()
+  ],
+  format: winston.format.combine(
+    winston.format.colorize(),
+    winston.format.json()
+  ),
+  meta: true, // optional: control whether you want to log the meta data about the request (default to true)
+  msg: "HTTP {{req.method}} {{req.url}}", // optional: customize the default logging message. E.g. "{{res.statusCode}} {{req.method}} {{res.responseTime}}ms {{req.url}}"
+  expressFormat: true, // Use the default Express/morgan request formatting. Enabling this will override any msg if true. Will only output colors with colorize set to true
+  colorize: false, // Color the text and status code, using the Express/morgan color palette (text: gray, status: default green, 3XX cyan, 4XX yellow, 5XX red).
+  ignoreRoute: function (req, res) { return false; } // optional: allows to skip some log messages based on request and/or response
+})
