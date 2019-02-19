1e2ad4fd4fbde3eead6cc96179cab1f1801eba60
ben-pr-p <ben.paul.ryan.packer@gmail.com>
2019-02-19 10:56:23 -0500

    add logs

--- src/api/test-pending-message-part.js
+++ src/api/test-pending-message-part.js
@@ -3,9 +3,14 @@ const { r } = require('../../models')
 
 async function main() {
   const pendingMessageParts = await r.knex('pending_message_part').select('*').limit(5)
+  console.log(pendingMessageParts)
   const finalMessage = await convertMessagePartsToMessage([part])
-  await saveNewIncomingMessage(finalMessage)
-  await r.knex('pending_message_part').where('id', partId).delete()
+  console.log(finalMessage)
+  const result = await saveNewIncomingMessage(finalMessage)
+  console.log(result)
+  const deleteResult = await r.knex('pending_message_part').where('id', partId).delete()
+  console.log(deleteResult)
+  return 'Success'
 }
 
 main().then(console.log).catch(console.error)
