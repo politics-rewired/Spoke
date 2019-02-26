ddc96393bd77eb41864ba4a531f0fd4435946b04
EC2 Default User <ec2-user@ip-10-0-1-74.ec2.internal>
2019-02-26 14:40:56 +0000

    add associate-messages script

---
+++ dev-tools/dedupe-campaigns.js
@@ -0,0 +1,155 @@
+const config = {
+    client: 'mysql',
+    connection: 'mysql://spoke:tHXtEb6YZ4P3SZfT9kzJyKykCg4mTbGgYsSTXUph4@bernie-pgbouncer.politicsrewired.com:4006/spoke_prod',
+    pool: {
+      min: 2,
+      max: 10
+    },
+  };
+  
+  const _ = require('lodash')
+  const db = require('knex')(config)
+  
+  // const badIds = [323883, 1045760, 122051, 330250, 330317, 325158]
+  
+  // // WHERE campaign_contact.id
+  // // NOT IN (${badIds.join(', ')})
+  
+  // /* Returns a list of n campaign contact duplicate pairs / trios */
+  // async function getNDuplicates(n, fromCampaigns) {
+  //   const whereClause = fromCampaigns ? `WHERE campaign_contact.campaign_id IN (${fromCampaigns.join(', ')})`
+  //   const res = await db.raw(`
+  //     SELECT campaign_contact.id,
+  //       campaign_contact.cell,
+  //       campaign_contact.message_status,
+  //       campaign_contact.campaign_id
+  //     FROM campaign_contact
+  //       INNER JOIN (
+  //         SELECT cell
+  //         FROM campaign_contact
+  //             GROUP BY cell
+  //             HAVING count(cell) > 1
+  //               AND SUM(
+  //                 CASE 
+  //                   WHEN message_status = 'needsMessage'
+  //                   THEN 1
+  //                   ELSE 0
+  //                 END
+  //               ) > 0
+  //             -- LIMIT ${n}
+  //       ) dup
+  //         ON campaign_contact.cell = dup.cell
+  //     ${whereClause};
+  //   `);
+  //   const rows = res[0]
+  //   console.log(rows.length)
+  //   return _.groupBy(rows, 'cell');
+  // }
+  
+  // /* Returns a list of campaign contacts with number `number` */
+  // async function getContactsWithNumber(number) {
+  //     const cells = await db('campaign_contact')
+  //         .where({ cell: number })
+  //         .pluck('cell');
+  //     return cells;
+  // }
+  
+  
+  // /* Deletes a list of campaign contacts */
+  // async function deleteCampaignContacts(toDelete) {
+  //   // console.log(`Would delete: ${JSON.stringify(toDelete.map(cc => cc.id))}`)
+  //   // return true
+  //   try {
+  //     const updatedToDelete = []
+  //     for (let cc of toDelete) {
+  //       if (!badIds.includes(cc.id)) {
+  //         updatedToDelete.push(cc.id)
+  //       }
+  //     }
+  
+  //     // const updatedToDelete = toDelete.map(cc => cc.id).filter(id => !badIds.includes(id))
+  //     const deleteResult = await db('campaign_contact').whereIn('id', updatedToDelete).del()
+  //   } catch (error) {
+  //     const failedId = error.sql.split(' ').reverse()[0].split('(')[1].split(')')[0]
+  //     badIds.push(parseInt(failedId))
+  //     console.log(`Got bad ID: ${parseInt(failedId)}`)
+  //     console.log(`We now have ${badIds.length} bad ids`)
+  //   }
+  //   // console.log({ deleteResult })
+  //   return true
+  // }
+  
+  // const BATCH_SIZE = 1000;
+  // let done = 1;
+  // async function main() {
+  //   let listOfDuplicates = await getNDuplicates(BATCH_SIZE, [66])
+  //   // console.log(listOfDuplicates)
+  //   // for (let phone of Object.keys(listOfDuplicates)) {
+  
+  //   await Promise.all(Object.keys(listOfDuplicates).map(async phone => {
+  //     const duplicates = listOfDuplicates[phone]
+  //     const hasReceivedMessage = duplicates.filter(cc => cc.message_status !== 'needsMessage').length > 0;
+  //     // console.log({ hasReceivedMessage })
+  //     if (hasReceivedMessage) {
+  //       const toDelete = duplicates.filter(cc => cc.message_status == 'needsMessage')
+  //       await deleteCampaignContacts(toDelete)
+  //     } else {
+  //       const sortedByCampaignId = _.sortBy(duplicates, cc => cc.campaign_id);
+  //       const toKeep = sortedByCampaignId[0];
+  //       // console.log(`Would keep: ${JSON.stringify(toKeep)}`)
+  //       const toDelete = sortedByCampaignId.slice(1, 10000);
+  //       await deleteCampaignContacts(toDelete)
+  //     }
+  //   }))
+  
+  //   console.log(`Did ${done * BATCH_SIZE}`)
+  //   done++;
+  
+  //   if (listOfDuplicates.length == 0) {
+  //     process.exit()
+  //   } else {
+  //     return await main()
+  //   }
+  //   // }
+  // }
+
+  async function go() {
+    const sql = `
+      SELECT campaign_contact.id
+      FROM campaign_contact
+      WHERE campaign_contact.campaign_id = 69
+        AND campaign_contact.cell IN (
+          SELECT cell
+          FROM campaign_contact
+          WHERE campaign_contact.campaign_id = 68
+        )
+    `;
+    const [ids, _] = await db.raw(sql)
+    const actualIds = ids.map(elem => elem.id);
+    const result = await db('campaign_contact')
+      .where({ campaign_id: 69 })
+      .whereIn('id', actualIds)
+      .del();
+    console.log(result);
+  }
+  
+  go()
+    .then(() => process.exit(0))
+    .catch(error => {
+      console.error(error);
+      process.exit(1);
+    });
+  
+  
+  // Psuedocode
+  /*
+  for phone_number in (select phone_number from campaign_contact where group by phone_number count > 1):
+    contacts = get all contacts with phone number `number`
+    hasRecievedMessage = (filter by contacts where status != needsMessage).length > 0
+    if (hasReceivedMessage)
+      unsent campaign contacts = filter contacts where campaign_contact = needsMessage
+      delete all unsent campaign contacts
+    else
+      select one contact to keep, the one with the lowest campaign id
+      delete others
+  */
---
+++ dev-tools/recover-question-responses.js
@@ -0,0 +1,99 @@
+const config = {
+  client: 'mysql',
+  connection: 'mysql://spoke:tHXtEb6YZ4P3SZfT9kzJyKykCg4mTbGgYsSTXUph4@bernie-pgbouncer.politicsrewired.com:4006/spoke_prod',
+  pool: {
+    min: 2,
+    max: 10
+  },
+};
+
+const campaign_contacts_without_question_responses = `
+create view campaign_contacts_without_question_responses as
+  select campaign_contact.id
+  from campaign_contact
+  where not exists (
+    select question_response.id
+    from question_response
+    where question_response.campaign_contact_id = campaign_contact.id
+  );
+`
+
+const question_response_messages = `
+create or replace view question_response_message_contact_numbers as 
+  select message.contact_number, message.text
+    from message
+    where message.text in (
+      select script
+      from interaction_step;
+    );
+`
+
+const drop_tables = `
+drop table question_response_message_contact_numbers_cached;
+drop table campaign_contacts_without_question_responses_cached;
+`
+
+const create_tables = `
+create table question_response_message_contact_numbers_cached select * from question_response_message_contact_numbers;
+create table campaign_contacts_without_question_responses_cached select * from campaign_contacts_without_question_responses;
+`
+
+const query = `
+select campaign_contact.id, campaign_contact.campaign_id, question_response_message_contact_numbers_cached.text
+from campaign_contact
+join question_response_message_contact_numbers_cached on question_response_message_contact_numbers_cached.contact_number = campaign_contact.cell
+where campaign_contact.id in (
+  select id
+  from campaign_contacts_without_question_responses_cached
+);
+`
+
+const db = require('knex')(config);
+
+let result
+let operatedOn = 0
+async function main() {
+  try {
+    await db.raw(create_tables)
+    console.log('Created tables')
+  } catch (ex) {
+    await db.raw(drop_tables)
+    console.log('Droppped tables')
+    await db.raw(create_tables)
+    console.log('Created tables')
+  }
+  result = await db.raw(query)
+  rows = result[0]
+  console.log(rows)
+  while (rows.length > 0) {
+    operatedOn = operatedOn + rows.length
+    for (let row of rows) {
+      await updateRow(row)
+    }
+    console.log(`Operating on ${operatedOn} rows`)
+    await db.raw(drop_tables)
+    console.log('Dropped tables')
+    await db.raw(create_tables)
+    console.log('Recreated tables')
+    result = await db.raw(query)
+    rows = result[0]
+    console.log(rows)
+  }
+  return operatedOn
+}
+
+async function updateRow({ id, campaign_id, text }) {
+  const matchingInteractionStep = await db('interaction_step').where({ script: text, campaign_id }).first()
+  if (matchingInteractionStep) {
+    const toInsert = {
+      interaction_step_id: matchingInteractionStep.id,
+      campaign_contact_id: id,
+      value: matchingInteractionStep.answer_option
+    }
+
+    const insert_result = await db('question_response').insert(toInsert)
+    console.log(95)
+  }
+}
+
+main().then(console.log).catch(console.error)
