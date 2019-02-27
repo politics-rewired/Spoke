20797970e73243d624dcb8bc79d2d77d9aa9d734
ben-pr-p <ben.paul.ryan.packer@gmail.com>
2019-02-26 20:12:20 -0500

    remove time constraints temporarily

--- src/server/api/assignment.js
+++ src/server/api/assignment.js
@@ -121,7 +121,7 @@ export async function giveUserMoreTexts(auth0Id, count) {
     throw new Error(`No user found with id ${auth0Id}`);
   }
 
-  const result = await r.knex.raw(`
+  const mainQuery = `
     select campaign_id
     from campaign_contact 
     join campaign on campaign_contact.campaign_id = campaign.id
@@ -131,7 +131,20 @@ export async function giveUserMoreTexts(auth0Id, count) {
     group by campaign_contact.campaign_id
     order by campaign.id
     limit 1;
-  `)
+  `
+
+  const withoutTimeConstraints = `
+    select campaign_id
+    from campaign_contact 
+    join campaign on campaign_contact.campaign_id = campaign.id
+    where assignment_id is null
+      and campaign.is_started = true and campaign.is_archived = false
+    group by campaign_contact.campaign_id
+    order by campaign.id
+    limit 1;
+  `
+
+  const result = await r.knex.raw(withoutTimeConstraints)
 
   /* Sample return:
     [ [ RowDataPacket { campaign_id: 1 } ],
