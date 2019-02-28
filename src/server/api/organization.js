6c5044f58aec6e81615c5bf4bcfd8b4bbc8289e4
ben-pr-p <ben.paul.ryan.packer@gmail.com>
2019-02-28 17:29:25 -0500

    just return true for now

--- src/server/api/organization.js
+++ src/server/api/organization.js
@@ -52,20 +52,21 @@ export const resolvers = {
       }
     },
     textsAvailable: async (organization) => {
-      const mainQuery = `
-        select campaign_id
-        from campaign_contact 
-        join campaign on campaign_contact.campaign_id = campaign.id
-        where assignment_id is null
-          and campaign.is_started = true and campaign.is_archived = false
-          and campaign.texting_hours_end > hour(CONVERT_TZ(UTC_TIMESTAMP(), 'UTC', campaign.timezone)) + 1
-        group by campaign_contact.campaign_id
-        order by campaign.id
-        limit 1;
-      `
+      // const mainQuery = `
+      //   select campaign_id
+      //   from campaign_contact 
+      //   join campaign on campaign_contact.campaign_id = campaign.id
+      //   where assignment_id is null
+      //     and campaign.is_started = true and campaign.is_archived = false
+      //     and campaign.texting_hours_end > hour(CONVERT_TZ(UTC_TIMESTAMP(), 'UTC', campaign.timezone)) + 1
+      //   group by campaign_contact.campaign_id
+      //   order by campaign.id
+      //   limit 1;
+      // `
 
-      const result = await r.knex.raw(mainQuery)
-      return result[0].length > 0;
+      // const result = await r.knex.raw(mainQuery)
+      // return result[0].length > 0;
+      return true
     }
   }
 }
