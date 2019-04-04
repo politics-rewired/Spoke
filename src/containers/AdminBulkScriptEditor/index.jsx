69bf9b886de50c96a96ac87a9144b02ac986a9d6
Benjamin Chrobot <benjamin.chrobot@alum.mit.edu>
2019-04-04 10:33:28 -0400

    Add admin navigation link to empty stub component.

---
+++ src/containers/AdminBulkScriptEditor/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const AdminBulkScriptEditor = (props) => {
+  return (
+    <h1>ToDo: Admin Bulk Script Editor</h1>
+  )
+}
+
+export default AdminBulkScriptEditor
