75c18f47adb0dc685a65d1cae6cd5ae289ee04b3
ben <ben.paul.ryan.packer@gmail.com>
2019-04-08 12:08:12 -0400

    parent_id -> parent_interaction_id

--- dev-tools/mysql-migrations/004-on-delete-cascade-interaction-step.sql
+++ dev-tools/mysql-migrations/004-on-delete-cascade-interaction-step.sql
@@ -2,6 +2,6 @@ ALTER TABLE interaction_step DROP FOREIGN KEY `interaction_step_parent_interacti
 
 ALTER TABLE interaction_step
    ADD CONSTRAINT `interaction_step_parent_interaction_id_index `
-   FOREIGN KEY (`parent_id` )
+   FOREIGN KEY (`parent_interaction_id` )
    REFERENCES `interaction_step` (`id` )
    ON DELETE CASCADE;
