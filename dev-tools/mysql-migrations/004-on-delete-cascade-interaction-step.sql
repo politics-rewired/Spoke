bbcabb8b687cd180c604c151b9ca31c61d73e633
ben <ben.paul.ryan.packer@gmail.com>
2019-04-08 12:11:14 -0400

    Proper name for new contraint

--- dev-tools/mysql-migrations/004-on-delete-cascade-interaction-step.sql
+++ dev-tools/mysql-migrations/004-on-delete-cascade-interaction-step.sql
@@ -1,7 +1,7 @@
 ALTER TABLE interaction_step DROP FOREIGN KEY `interaction_step_parent_interaction_id_foreign`;
 
 ALTER TABLE interaction_step
-   ADD CONSTRAINT `interaction_step_parent_interaction_id_index `
+   ADD CONSTRAINT `interaction_step_parent_interaction_id_foreign `
    FOREIGN KEY (`parent_interaction_id` )
    REFERENCES `interaction_step` (`id` )
    ON DELETE CASCADE;
