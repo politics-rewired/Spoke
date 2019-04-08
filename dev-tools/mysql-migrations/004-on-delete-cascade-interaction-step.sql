7b7d61b950e0f6f6e9895699bf4b8ba337130abc
ben <ben.paul.ryan.packer@gmail.com>
2019-04-08 12:10:32 -0400

    Drop foreign key, not index

--- dev-tools/mysql-migrations/004-on-delete-cascade-interaction-step.sql
+++ dev-tools/mysql-migrations/004-on-delete-cascade-interaction-step.sql
@@ -1,4 +1,4 @@
-ALTER TABLE interaction_step DROP FOREIGN KEY `interaction_step_parent_interaction_id_index`;
+ALTER TABLE interaction_step DROP FOREIGN KEY `interaction_step_parent_interaction_id_foreign`;
 
 ALTER TABLE interaction_step
    ADD CONSTRAINT `interaction_step_parent_interaction_id_index `
