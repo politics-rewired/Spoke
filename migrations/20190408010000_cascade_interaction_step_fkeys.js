fd27044dbc9a91afbd2c2b29a271022ea8027da7
Benjamin Chrobot <benjamin.chrobot@alum.mit.edu>
2019-04-25 15:51:39 -0400

    Add MySQL migrations not already bundled in base Knex-style migration.

---
+++ migrations/20190408010000_cascade_interaction_step_fkeys.js
@@ -0,0 +1,15 @@
+// Cascade deletes from parent interaction steps down to children
+exports.up = function(knex, Promise) {
+  return knex.schema.alterTable('interaction_step', table => {
+    table.foreign('parent_interaction_id')
+      .references('interaction_step.id')
+      .onDelete('CASCADE')
+  })
+}
+
+// Remove cascading deletes from interaction steps
+exports.down = function(knex, Promise) {
+  return knex.schema.alterTable('interaction_step', table => {
+    table.foreign('parent_interaction_id').references('interaction_step.id')
+  })
+}
