4ab696cec31445507cbf31ecda28a09ea3e1a8a4
ben <ben.paul.ryan.packer@gmail.com>
2019-06-19 09:55:39 -0400

    Update recover-question-responses.js

--- dev-tools/recover-question-responses.js
+++ dev-tools/recover-question-responses.js
@@ -1,7 +1,7 @@
 const config = {
   client: "mysql",
   connection:
-    "mysql://spoke:tHXtEb6YZ4P3SZfT9kzJyKykCg4mTbGgYsSTXUph4@bernie-pgbouncer.politicsrewired.com:4006/spoke_prod",
+    process.env.DATABASE_URL,
   pool: {
     min: 2,
     max: 10
