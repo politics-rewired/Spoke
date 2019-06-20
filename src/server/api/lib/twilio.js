5012c0b8be2dfa083040b53073cf37675d28f891
Benjamin Chrobot <benjamin.chrobot@alum.mit.edu>
2019-06-20 15:56:10 -0400

    refactor: clean up Twilio's sendMessage()

--- src/server/api/lib/twilio.js
+++ src/server/api/lib/twilio.js
@@ -11,8 +11,8 @@ import {
 import { symmetricDecrypt } from "./crypto";
 
 const MAX_SEND_ATTEMPTS = 5;
-const MESSAGE_VALIDITY_PADDING_SECONDS = 30;
-const MAX_TWILIO_MESSAGE_VALIDITY = 14400;
+const MESSAGE_VALIDITY_PADDING_S = 30;
+const MAX_TWILIO_MESSAGE_VALIDITY = 60 * 60 * 4; // 4 hours
 
 const headerValidator = () => {
   const {
@@ -228,7 +228,23 @@ const twilioClient = async messagingServiceSid => {
   return Twilio(accountSid, authToken);
 };
 
+const getValidityPeriod = sendBefore => {
+  // Calculate the number of seconds until sendBefore and subtract a small padding buffer.
+  // TODO - this is local timezone, not UTC
+  const secondsUntilSendBefore = Math.floor((sendBefore - Date.now()) / 1000);
+  const validityPeriod = secondsUntilSendBefore - MESSAGE_VALIDITY_PADDING_S;
+
+  const maxValidityPeriod = process.env.TWILIO_MESSAGE_VALIDITY_PERIOD
+    ? process.env.TWILIO_MESSAGE_VALIDITY_PERIOD
+    : MAX_TWILIO_MESSAGE_VALIDITY;
+
+  // If this is less than 0, then sendMessage was already called too late.
+  // Pass the buck and let Twilio respond with error
+  return Math.min(validityPeriod, maxValidityPeriod);
+};
+
 async function sendMessage(message, organizationId, trx) {
+  const knexObject = trx || r.knex;
   // Get (or assign) messaging service for contact's number
   const messagingServiceSid = await getMessageServiceSID(
     message.contact_number,
@@ -236,131 +252,40 @@ async function sendMessage(message, organizationId, trx) {
   );
   const twilio = await twilioClient(messagingServiceSid);
 
-  if (!twilio) {
-    log.warn(
-      "cannot actually send SMS message -- twilio is not fully configured:",
-      message.id
-    );
-    if (message.id) {
-      const options = trx ? { transaction: trx } : {};
-      await Message.get(message.id).update(
-        { send_status: "SENT", sent_at: new Date() },
-        options
-      );
+  const { send_before: sendBefore, contact_number: contactNumber } = message;
+  const { body, mediaUrl } = parseMessageText(message);
+  const validityPeriod = sendBefore ? getValidityPeriod(sendBefore) : undefined;
+  const messageInstanceOptions = {
+    to: contactNumber,
+    messagingServiceSid,
+    validityPeriod,
+    body,
+    mediaUrl
+  };
+
+  const updates = { service_response: message.service_response };
+  try {
+    const response = await twilio.messages.create(messageInstanceOptions);
+    if (response.error_code) throw new Error(response);
+    updates.service_id = response.sid;
+    updates.service_response += JSON.stringify(response);
+    updates.send_status = "SENT";
+    updates.service = "twilio";
+    updates.sent_at = knexObject.fn.now();
+  } catch (exc) {
+    updates.service_response += JSON.stringify(err);
+
+    const statusCount = messageToSave.service_response.split("status").length;
+    if (statusCount >= MAX_SEND_ATTEMPTS + 1) {
+      updates.send_status = "ERROR";
     }
-    return "test_message_uuid";
+  } finally {
+    const { rows } = await knexObject("message")
+      .update(updates)
+      .where({ id: message.id })
+      .returning("*");
+    return rows[0];
   }
-
-  return new Promise(async (resolve, reject) => {
-    if (message.service !== "twilio") {
-      log.warn("Message not marked as a twilio message", message.id);
-    }
-
-    const messageParams = Object.assign(
-      {
-        to: message.contact_number,
-        body: message.text,
-        messagingServiceSid: messagingServiceSid,
-        statusCallback: process.env.TWILIO_STATUS_CALLBACK_URL
-      },
-      parseMessageText(message)
-    );
-
-    let twilioValidityPeriod = process.env.TWILIO_MESSAGE_VALIDITY_PERIOD;
-
-    if (message.send_before) {
-      // the message is valid no longer than the time between now and
-      // the send_before time, less 30 seconds
-      // we subtract the MESSAGE_VALIDITY_PADDING_SECONDS seconds to allow time for the message to be sent by
-      // a downstream service
-      // TODO - this is local timezone, not UTC
-      const messageValidityPeriod =
-        Math.ceil((message.send_before - Date.now()) / 1000) -
-        MESSAGE_VALIDITY_PADDING_SECONDS;
-
-      if (messageValidityPeriod < 0) {
-        // this is an edge case
-        // it means the message arrived in this function already too late to be sent
-        // pass the negative validity period to twilio, and let twilio respond with an error
-      }
-
-      if (twilioValidityPeriod) {
-        twilioValidityPeriod = Math.min(
-          twilioValidityPeriod,
-          messageValidityPeriod,
-          MAX_TWILIO_MESSAGE_VALIDITY
-        );
-      } else {
-        twilioValidityPeriod = Math.min(
-          messageValidityPeriod,
-          MAX_TWILIO_MESSAGE_VALIDITY
-        );
-      }
-    }
-
-    if (twilioValidityPeriod) {
-      messageParams.validityPeriod = twilioValidityPeriod;
-    }
-
-    twilio.messages.create(messageParams, (err, response) => {
-      const messageToSave = {
-        ...message
-      };
-      log.info("messageToSave", messageToSave);
-      let hasError = false;
-      if (err) {
-        hasError = true;
-        log.error("Error sending message", err);
-        console.error(err);
-        messageToSave.service_response += JSON.stringify(err);
-      }
-      if (response) {
-        messageToSave.service_id = response.sid;
-        hasError = !!response.error_code;
-        messageToSave.service_response += JSON.stringify(response);
-      }
-
-      if (hasError) {
-        const SENT_STRING = '"status"'; // will appear in responses
-        if (
-          messageToSave.service_response.split(SENT_STRING).length >=
-          MAX_SEND_ATTEMPTS + 1
-        ) {
-          messageToSave.send_status = "ERROR";
-        }
-        let options = { conflict: "update" };
-        if (trx) {
-          options.transaction = trx;
-        }
-        Message.save(messageToSave, options)
-          // eslint-disable-next-line no-unused-vars
-          .then((_, newMessage) => {
-            reject(
-              err ||
-                (response
-                  ? new Error(JSON.stringify(response))
-                  : new Error("Encountered unknown error"))
-            );
-          });
-      } else {
-        let options = { conflict: "update" };
-        if (trx) {
-          options.transaction = trx;
-        }
-        Message.save(
-          {
-            ...messageToSave,
-            send_status: "SENT",
-            service: "twilio",
-            sent_at: new Date()
-          },
-          options
-        ).then((saveError, newMessage) => {
-          resolve(newMessage);
-        });
-      }
-    });
-  });
 }
 
 // Get appropriate Spoke message status from Twilio status
