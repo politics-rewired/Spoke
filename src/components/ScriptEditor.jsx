9a101a9673d62f57132d2aa2a14eafb12799818c
Benjamin Chrobot <benjamin.chrobot@alum.mit.edu>
2019-06-26 08:54:35 -0400

    Add missing key to mapped nodes.

--- src/components/ScriptEditor.jsx
+++ src/components/ScriptEditor.jsx
@@ -176,6 +176,7 @@ class ScriptEditor extends React.Component {
       <div style={styles.scriptFieldButtonSection}>
         {scriptFields.map(field => (
           <Chip
+            key={field}
             style={styles.scriptFieldButton}
             text={delimit(field)}
             onTouchTap={() => this.addCustomField(field)}
