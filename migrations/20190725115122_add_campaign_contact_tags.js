ede1fba57b0009c3bdf6b7808b28ba0b2592b3ec
Benjamin Chrobot <benjamin.chrobot@alum.mit.edu>
2019-07-25 12:10:05 -0400

    feat(tags): add migration for tags

---
+++ migrations/20190725115122_add_campaign_contact_tags.js
@@ -0,0 +1,68 @@
+exports.up = function(knex, Promise) {
+  return knex.schema
+    .createTable("tag", table => {
+      table.increments("id").primary();
+      table
+        .integer("organization_id")
+        .unsigned()
+        .notNullable();
+      table
+        .text("title")
+        .notNullable()
+        .unique();
+      table
+        .text("description")
+        .notNullable()
+        .default("");
+      table
+        .integer("author_id")
+        .unsigned()
+        .notNullable();
+      table
+        .boolean("is_assignable")
+        .notNullable()
+        .default(true);
+      table
+        .boolean("is_system")
+        .notNullable()
+        .default(false);
+      table
+        .timestamp("created_at")
+        .notNullable()
+        .defaultTo(knex.fn.now());
+
+      table.foreign("organization_id").references("organization.id");
+      table.foreign("author_id").references("user.id");
+    })
+    .then(() =>
+      knex.schema.createTable("campaign_contact_tag", table => {
+        table
+          .integer("campaign_contact_id")
+          .unsigned()
+          .notNullable();
+        table
+          .integer("tag_id")
+          .unsigned()
+          .notNullable();
+        table
+          .integer("tagger_id")
+          .unsigned()
+          .notNullable();
+        table
+          .timestamp("created_at")
+          .notNullable()
+          .defaultTo(knex.fn.now());
+
+        table.primary(["campaign_contact_id", "tag_id"]);
+        table.foreign("campaign_contact_id").references("campaign_contact.id");
+        table.foreign("tag_id").references("tag.id");
+        table.foreign("tagger_id").references("user.id");
+      })
+    );
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema
+    .dropTable("campaign_contact_tag")
+    .then(() => knex.schema.dropTable("tag"));
+};
