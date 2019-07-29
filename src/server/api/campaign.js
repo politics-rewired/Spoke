45b526ae4c7058be3772aa9527650f14b44e235f
ben <ben.paul.ryan.packer@gmail.com>
2019-07-29 15:07:03 -0400

    Update src/server/api/campaign.js
    Co-Authored-By: Benjamin Chrobot <benjamin.chrobot@alum.mit.edu>

--- src/server/api/campaign.js
+++ src/server/api/campaign.js
@@ -260,7 +260,7 @@ export const resolvers = {
         .from("campaign_contact_tag")
         .join("tag", "tag.id", "=", "campaign_contact_tag.tag_id")
         .where({
-          "tag.title": "Escalated",
+          "lower(tag.title)": "escalated",
           "tag.organization_id": campaign.organization_id
         })
         .whereRaw(
