409049c1399aae42caba6a3f3ffdabd2bcdc56b7
Benjamin Chrobot <benjamin.chrobot@alum.mit.edu>
2019-08-06 12:25:16 -0400

    feat(export): create export drivers for AWS and GCS

---
+++ src/workers/exports/gs-json.js
@@ -0,0 +1,69 @@
+const { Storage } = require("@google-cloud/storage");
+
+const fetchKeys = () => {
+  const keysEnvVar = process.env["GOOGLE_APPLICATION_CREDENTIALS"];
+  if (!keysEnvVar) {
+    throw new Error(
+      "The $GOOGLE_APPLICATION_CREDENTIALS environment variable was not found!"
+    );
+  }
+  const keys = JSON.parse(keysEnvVar);
+  return keys;
+};
+
+let _storage = undefined;
+const storage = () => {
+  if (_storage === undefined) {
+    const keys = fetchKeys();
+    const credentials = {
+      client_email: keys.client_email,
+      private_key: keys.private_key
+    };
+    _storage = new Storage({ credentials, projectId: keys.project_id });
+  }
+  return _storage;
+};
+
+/**
+ * Upload a payload to Google Cloud Storage.
+ *
+ * @param {string} bucket Name of the GCS bucket to upload the object to
+ * @param {string} key Name of key of destination object
+ * @param {Buffer|Uint8Array|Blob|string|Readable} payload Payload to upload
+ */
+const upload = async (bucket, key, payload) => {
+  return new Promise(async (resolve, reject) => {
+    const uploadStream = await storage()
+      .bucket(bucket)
+      .file(key)
+      .createWriteStream({ gzip: true });
+    uploadStream.write(payload);
+    uploadStream.end();
+    uploadStream.on("finish", resolve);
+    uploadStream.on("error", reject);
+  });
+};
+
+/**
+ * Get a signed URL for an object that is valid for 24 hours.
+ *
+ * @param {string} bucket Name of the S3 bucket to upload the object to
+ * @param {string} key Name of key of destination object
+ * @returns {string} Signed download URL
+ */
+const getDownloadUrl = async (bucket, key) => {
+  const [url] = await storage()
+    .bucket(bucket)
+    .file(key)
+    .getSignedUrl({
+      version: "v4",
+      action: "read",
+      expires: Date.now() + 1000 * 60 * 60 * 24 // 24 hours
+    });
+  return url;
+};
+
+module.exports = {
+  upload,
+  getDownloadUrl
+};
---
+++ src/workers/exports/s3.js
@@ -0,0 +1,48 @@
+const AWS = require("aws-sdk");
+
+const { AWS_ENDPOINT: awsEndpoint } = process.env;
+
+const createS3 = (bucket, endpointUrl = awsEndpoint) => {
+  let endpoint = undefined;
+  if (endpointUrl) {
+    endpoint = new AWS.Endpoint(endpointUrl);
+  }
+  const s3Client = new AWS.S3({
+    signatureVersion: "v4",
+    params: { Bucket: bucket },
+    endpoint
+  });
+  return s3Client;
+};
+
+/**
+ * Upload a payload to AWS S3.
+ *
+ * @param {string} bucket Name of the S3 bucket to upload the object to
+ * @param {string} key Name of key of destination object
+ * @param {Buffer|Uint8Array|Blob|string|Readable} payload Payload to upload
+ */
+const upload = async (bucket, key, payload, endpointUrl) => {
+  const s3Client = createS3(bucket, endpointUrl);
+  const uploadParams = { Key: key, Body: payload };
+  return s3Client.putObject(uploadParams).promise();
+};
+
+/**
+ * Get a signed URL for an object that is valid for 24 hours.
+ *
+ * @param {string} bucket Name of the S3 bucket to upload the object to
+ * @param {string} key Name of key of destination object
+ * @returns {string} Signed download URL
+ */
+const getDownloadUrl = async (bucket, key) => {
+  const s3Client = createS3(bucket);
+  const expiresSeconds = 60 * 60 * 24;
+  const options = { Key: key, Expires: expiresSeconds };
+  return s3Client.getSignedUrl("getObject", options);
+};
+
+module.exports = {
+  upload,
+  getDownloadUrl
+};
