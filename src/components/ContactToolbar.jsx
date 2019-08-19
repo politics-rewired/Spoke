5ee63c3137a524462743b396921798625a67c59e
Benjamin Chrobot <benjamin.chrobot@alum.mit.edu>
2019-08-19 14:40:59 -0400

    fix(logging): remove unused client-side reference to server function

--- src/components/ContactToolbar.jsx
+++ src/components/ContactToolbar.jsx
@@ -1,7 +1,6 @@
 import PropTypes from "prop-types";
 import React from "react";
 import { Toolbar, ToolbarGroup, ToolbarTitle } from "material-ui/Toolbar";
-import { getDisplayPhoneNumber } from "../lib/phone-format";
 import { getLocalTime, getContactTimezone } from "../lib/timezones";
 import { getProcessEnvDstReferenceTimezone } from "../lib/tz-helpers";
 import { grey100 } from "material-ui/styles/colors";
