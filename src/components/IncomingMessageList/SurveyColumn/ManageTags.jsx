22838b9170fde6700fd5fa3eff2b5002fce4b868
Benjamin Chrobot <benjamin.chrobot@alum.mit.edu>
2019-08-28 14:53:01 -0400

    feat(tag): add tag management to sweeper view

---
+++ src/components/IncomingMessageList/SurveyColumn/ManageTags.jsx
@@ -0,0 +1,182 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import gql from "graphql-tag";
+import isEqual from "lodash/isEqual";
+
+import RaisedButton from "material-ui/RaisedButton";
+import FlatButton from "material-ui/FlatButton";
+import Dialog from "material-ui/Dialog";
+
+import loadData from "../../../containers/hoc/load-data";
+import TagSelector from "../../TagSelector";
+
+class ManageTags extends Component {
+  state = {
+    isTagEditorOpen: false,
+    selectedTags: [],
+    isWorking: false,
+    error: undefined
+  };
+
+  componentWillMount() {
+    this.state.selectedTags = [...this.props.contactTags.contact.tags];
+  }
+
+  componentWillReceiveProps(nextProps) {
+    const { tags: oldTags } = this.props.contactTags.contact;
+    const { tags: newTags } = nextProps.contactTags.contact;
+    if (!isEqual(oldTags, newTags)) {
+      this.state.selectedTags = [...newTags];
+    }
+  }
+
+  handleOnClickEditTags = () => this.setState({ isTagEditorOpen: true });
+
+  handleCloseTagManager = () => this.setState({ isTagEditorOpen: false });
+
+  handleOnChangeTags = selectedTags => this.setState({ selectedTags });
+
+  handleSaveTags = async () => {
+    const { tags } = this.props.contactTags.contact;
+    const { selectedTags } = this.state;
+    const contactTagIds = new Set(tags.map(tag => tag.id));
+    const selectedTagIds = new Set(selectedTags.map(tag => tag.id));
+    const addedTags = selectedTags.filter(tag => !contactTagIds.has(tag.id));
+    const removedTags = tags.filter(tag => !selectedTagIds.has(tag.id));
+    const tagPayload = {
+      addedTagIds: addedTags.map(tag => tag.id),
+      removedTagIds: removedTags.map(tag => tag.id)
+    };
+
+    this.setState({ isWorking: true });
+    try {
+      const response = await this.props.mutations.tagContact(tagPayload);
+      if (response.errors) throw response.errors;
+      this.handleCloseTagManager();
+    } catch (err) {
+      this.setState({ error: err.message });
+    } finally {
+      this.setState({ isWorking: false });
+    }
+  };
+
+  render() {
+    const { tagList } = this.props.organizationTags.organization;
+    const { tags: contactTags } = this.props.contactTags.contact;
+    const { isTagEditorOpen, selectedTags, isWorking } = this.state;
+
+    const actions = [
+      <RaisedButton
+        label="Save"
+        primary={true}
+        disabled={isWorking}
+        onClick={this.handleSaveTags}
+      />,
+      <FlatButton label="Cancel" onClick={this.handleCloseTagManager} />
+    ];
+
+    return (
+      <div style={{ textAlign: "right" }}>
+        <RaisedButton
+          label={`Edit Tags (${contactTags.length})`}
+          disabled={isWorking}
+          onClick={this.handleOnClickEditTags}
+        />
+        <Dialog
+          title="Manage Tags"
+          actions={actions}
+          open={isTagEditorOpen}
+          modal={false}
+          onRequestClose={this.handleCloseTagManager}
+        >
+          <TagSelector
+            dataSource={tagList}
+            value={selectedTags}
+            onChange={this.handleOnChangeTags}
+          />
+        </Dialog>
+      </div>
+    );
+  }
+}
+
+ManageTags.defaultProps = {};
+
+ManageTags.propTypes = {
+  organizationId: PropTypes.string.isRequired,
+  organizationTags: PropTypes.object.isRequired,
+  contactId: PropTypes.string.isRequired,
+  contactTags: PropTypes.object.isRequired
+};
+
+const mapQueriesToProps = ({ ownProps }) => ({
+  organizationTags: {
+    query: gql`
+      query getOrganizationTags($organizationId: String!) {
+        organization(id: $organizationId) {
+          id
+          tagList {
+            id
+            title
+            description
+            confirmationSteps
+            onApplyScript
+            isSystem
+            isAssignable
+            createdAt
+          }
+        }
+      }
+    `,
+    variables: {
+      organizationId: ownProps.organizationId
+    },
+    forceFetch: true
+  },
+  contactTags: {
+    query: gql`
+      query getContactTags($contactId: String!) {
+        contact(id: $contactId) {
+          id
+          tags: contactTags {
+            id
+            title
+            description
+            confirmationSteps
+            onApplyScript
+            isSystem
+            isAssignable
+            createdAt
+          }
+        }
+      }
+    `,
+    variables: {
+      contactId: ownProps.contactId
+    },
+    forceFetch: true
+  }
+});
+
+const mapMutationsToProps = ({ ownProps }) => ({
+  tagContact: tagPayload => ({
+    mutation: gql`
+      mutation tagConversation(
+        $contactId: String!
+        $tagPayload: ContactTagActionInput!
+      ) {
+        tagConversation(campaignContactId: $contactId, tag: $tagPayload) {
+          id
+          assignmentId
+        }
+      }
+    `,
+    variables: {
+      contactId: ownProps.contactId,
+      tagPayload
+    },
+    refetchQueries: ["getContactTags"]
+  })
+});
+
+export default loadData(ManageTags, { mapQueriesToProps, mapMutationsToProps });
--- src/components/IncomingMessageList/SurveyColumn/index.jsx
+++ src/components/IncomingMessageList/SurveyColumn/index.jsx
@@ -1,245 +1,33 @@
-import React, { Component } from "react";
-import gql from "graphql-tag";
-import { connect } from "react-apollo";
-import SelectField from "material-ui/SelectField";
-import MenuItem from "material-ui/MenuItem";
-import Dialog from "material-ui/Dialog";
-import FlatButton from "material-ui/FlatButton";
+import React from "react";
+import PropTypes from "prop-types";
 
-import LoadingIndicator from "../../LoadingIndicator";
+import ManageSurveyResponses from "./ManageSurveyResponses";
+import ManageTags from "./ManageTags";
 
-class SurveyColumn extends Component {
-  state = {
-    isMakingRequest: false,
-    requestError: "",
-    questionResponses: {}
-  };
+const styles = {
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    height: "100%"
+  },
+  spacer: { flex: "1" }
+};
 
-  componentWillMount() {
-    const { interactionSteps } = this.props.campaign;
-    const questionResponses = interactionSteps
-      .filter(iStep => iStep.questionResponse)
-      .reduce((collector, iStep) => {
-        collector[iStep.id] = iStep.questionResponse.value;
-        return collector;
-      }, {});
-    this.state.questionResponses = questionResponses;
-  }
-
-  getResponsesFrom = startingStepId => {
-    const { interactionSteps } = this.props.campaign;
-    const { questionResponses } = this.state;
-
-    const iSteps = [];
-    let currentStep = interactionSteps.find(
-      iStep => iStep.questionText && iStep.id === startingStepId
-    );
-    while (currentStep) {
-      const children = interactionSteps.filter(
-        iStep => iStep.parentInteractionId === currentStep.id
-      );
-      iSteps.push(Object.assign({}, currentStep, { children }));
-      const value = questionResponses[currentStep.id];
-      currentStep = value
-        ? // Only show actionable questions
-          interactionSteps.find(
-            iStep => iStep.questionText && iStep.answerOption === value
-          )
-        : null;
-    }
-    return iSteps;
-  };
-
-  createHandler = iStepId => {
-    const {
-      updateQuestionResponses,
-      deleteQuestionResponses
-    } = this.props.mutations;
-    return async (event, index, value) => {
-      this.setState({ isMakingRequest: true });
-      const { contact } = this.props;
-      let { questionResponses } = this.state;
-      const affectedSteps = this.getResponsesFrom(iStepId);
-
-      try {
-        // Delete response for this and all children (unless this is a single question change)
-        if (affectedSteps.length > 1 || !value) {
-          const iStepIds = affectedSteps.map(iStep => iStep.id);
-          const response = await deleteQuestionResponses(iStepIds, contact.id);
-          if (response.errors) throw response.errors;
-          iStepIds.forEach(iStepId => delete questionResponses[iStepId]);
-        }
-
-        if (value) {
-          const input = {
-            campaignContactId: contact.id,
-            interactionStepId: iStepId,
-            value
-          };
-          const response = await updateQuestionResponses([input], contact.id);
-          if (response.errors) throw response.errors;
-          questionResponses[iStepId] = value;
-        }
-      } catch (error) {
-        this.setState({ requestError: error.message });
-      } finally {
-        this.setState({
-          isMakingRequest: false,
-          questionResponses
-        });
-      }
-    };
-  };
-
-  handleCloseError = () => {
-    this.setState({ requestError: "" });
-  };
-
-  render() {
-    const { interactionSteps } = this.props.campaign;
-    const { isMakingRequest, questionResponses } = this.state;
-
-    let startingStep = interactionSteps.find(
-      iStep => iStep.parentInteractionId === null
-    );
-    const renderSteps = this.getResponsesFrom(startingStep.id);
-
-    if (renderSteps.length === 0) {
-      return <p>No answerable questions for this campaign.</p>;
-    }
-
-    const errorActions = [
-      <FlatButton label="OK" primary={true} onClick={this.handleCloseError} />
-    ];
-
-    return (
-      <div style={{ maxHeight: "400px", overflowY: "scroll" }}>
-        {renderSteps.map(iStep => {
-          const responseValue = questionResponses[iStep.id];
-          return (
-            <SelectField
-              key={iStep.id}
-              floatingLabelText={iStep.questionText}
-              value={responseValue}
-              disabled={isMakingRequest}
-              onChange={this.createHandler(iStep.id)}
-              style={{ width: "100%" }}
-            >
-              <MenuItem value={null} primaryText="" />
-              {iStep.children.map(option => (
-                <MenuItem
-                  key={option.answerOption}
-                  value={option.answerOption}
-                  primaryText={option.answerOption}
-                />
-              ))}
-            </SelectField>
-          );
-        })}
-        <Dialog
-          title="Error Updating Question Response"
-          actions={errorActions}
-          modal={false}
-          open={!!this.state.requestError}
-          onRequestClose={this.handleCloseError}
-        >
-          {this.state.requestError}
-        </Dialog>
-      </div>
-    );
-  }
-}
-
-const SurveyColumnWrapper = props => {
-  const { surveyQuestions, mutations, contact } = props;
+const SurveyColumn = props => {
+  const { campaign, contact, organizationId } = props;
   return (
-    <div>
-      <h4>Survey Responses</h4>
-      {surveyQuestions.loading && <LoadingIndicator />}
-      {surveyQuestions.errors && <p>{surveyQuestions.errors.message}</p>}
-      {surveyQuestions.campaign && (
-        <SurveyColumn
-          campaign={surveyQuestions.campaign}
-          contact={contact}
-          mutations={mutations}
-        />
-      )}
+    <div style={styles.container}>
+      <ManageSurveyResponses contact={contact} campaign={campaign} />
+      <div style={styles.spacer} />
+      <ManageTags organizationId={organizationId} contactId={contact.id} />
     </div>
   );
 };
 
-const mapQueriesToProps = ({ ownProps }) => ({
-  surveyQuestions: {
-    query: gql`
-      query getSurveyQuestions($campaignId: String!, $contactId: String!) {
-        campaign(id: $campaignId) {
-          id
-          interactionSteps {
-            id
-            questionText
-            answerOption
-            parentInteractionId
-            isDeleted
-            answerActions
+SurveyColumn.propTypes = {
+  campaign: PropTypes.object.isRequired,
+  contact: PropTypes.object.isRequired,
+  organizationId: PropTypes.string.isRequired
+};
 
-            questionResponse(campaignContactId: $contactId) {
-              id
-              value
-            }
-          }
-        }
-      }
-    `,
-    variables: {
-      campaignId: ownProps.campaign.id,
-      contactId: ownProps.contact.id
-    },
-    forceFetch: true
-  }
-});
-
-const mapMutationsToProps = () => ({
-  updateQuestionResponses: (questionResponses, campaignContactId) => ({
-    mutation: gql`
-      mutation updateQuestionResponses(
-        $questionResponses: [QuestionResponseInput]
-        $campaignContactId: String!
-      ) {
-        updateQuestionResponses(
-          questionResponses: $questionResponses
-          campaignContactId: $campaignContactId
-        ) {
-          id
-        }
-      }
-    `,
-    variables: {
-      questionResponses,
-      campaignContactId
-    }
-  }),
-  deleteQuestionResponses: (interactionStepIds, campaignContactId) => ({
-    mutation: gql`
-      mutation deleteQuestionResponses(
-        $interactionStepIds: [String]
-        $campaignContactId: String!
-      ) {
-        deleteQuestionResponses(
-          interactionStepIds: $interactionStepIds
-          campaignContactId: $campaignContactId
-        ) {
-          id
-        }
-      }
-    `,
-    variables: {
-      interactionStepIds,
-      campaignContactId
-    }
-  })
-});
-
-export default connect({
-  mapQueriesToProps,
-  mapMutationsToProps
-})(SurveyColumnWrapper);
+export default SurveyColumn;
