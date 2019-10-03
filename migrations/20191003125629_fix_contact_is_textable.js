3848c0b32e5781dee591c82fce4c88820f1f6c7c
ben-pr-p <ben.paul.ryan.packer@gmail.com>
2019-10-03 12:57:46 -0400

    fix: contact is textable function

---
+++ migrations/20191003125629_fix_contact_is_textable.js
@@ -0,0 +1,23 @@
+exports.up = function(knex) {
+  return knex.schema.raw(`
+    create or replace function contact_is_textable_now(timezone text, start integer, stop integer, allow_null boolean) returns boolean as $$
+      select (timezone is null and allow_null)
+        or (
+          extract(hour from (CURRENT_TIMESTAMP at time zone timezone)) >= start
+          and extract(hour from (CURRENT_TIMESTAMP at time zone timezone)) <= stop
+        )
+    $$ language sql;
+  `);
+};
+
+exports.down = function(knex) {
+  return knex.schema.raw(`
+    create or replace function contact_is_textable_now(timezone text, start integer, stop integer, allow_null boolean) returns boolean as $$
+      select (timezone is null and allow_null)
+        or (
+          extract(hour from (CURRENT_TIMESTAMP at time zone timezone)) > start
+          and extract(hour from (CURRENT_TIMESTAMP at time zone timezone)) < stop
+        )
+    $$ language sql;
+  `);
+};
