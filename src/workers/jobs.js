13fd5f2b2c0d533392a68a36ae6d021cb46477a6
ben-pr-p <ben.paul.ryan.packer@gmail.com>
2019-11-17 13:03:24 -0500

    fix: messages export blank

--- src/workers/jobs.js
+++ src/workers/jobs.js
@@ -1222,6 +1222,11 @@ const processMessagesChunk = async (campaignId, lastContactId = 0) => {
         where
           campaign_id = ?
           and id > ?
+          and exists (
+            select 1
+            from message
+            where campaign_contact_id = campaign_contact.id
+          )
         order by
           id asc
         limit ?
@@ -1307,6 +1312,7 @@ export async function exportCampaign(job) {
     notificationEmail = undefined,
     interactionSteps = undefined,
     assignments = undefined;
+
   try {
     ({
       campaignId,
