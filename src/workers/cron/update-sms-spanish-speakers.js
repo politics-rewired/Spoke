ce098389f0a252d327ae1354f7a6368a03b7a83c
ben-pr-p <ben.paul.ryan.packer@gmail.com>
2020-01-06 15:00:06 -0500

    feat: update texter name in external assignment manager db cron job

---
+++ src/workers/cron/update-sms-spanish-speakers.js
@@ -0,0 +1,49 @@
+import knex from "knex";
+import logger from "../../logger";
+import { config } from "../../config";
+import knexConfig from "../../server/knex";
+
+const TEAM_ID = config.SPANISH_TEAM_ID;
+
+const spokeDb = knex(knexConfig);
+const assignmentManagerDb = knex({
+  client: "mysql",
+  connection: config.ASSIGNMENT_MANAGER_DATABASE_URL
+});
+
+const main = async () => {
+  const now = new Date();
+  now.setMinutes(now.getMinutes() - 5.5);
+  const fiveAndAHalfMinutesAgo = now;
+
+  const newAssignmentUsers = await assignmentManagerDb("berniesms_shiftrequest")
+    .select("auth_user.email")
+    .join("auth_user", "auth_user.id", "=", "user_id")
+    .where(
+      "berniesms_shiftrequest.created",
+      ">",
+      fiveAndAHalfMinutesAgo.toISOString()
+    );
+
+  const membersOfTeam = await spokeDb("user_team")
+    .select("user.email")
+    .join("user", "id", "=", "user_id")
+    .where({ team_id: TEAM_ID })
+    .whereIn("email", newAssignmentUsers.map(na => na.email));
+
+  for (let member of membersOfTeam) {
+    const assignmentManagerDbUser = newAssignmentUsers.find(
+      na => na.email === member.email
+    );
+
+    await assignmentManagerDb("auth_user")
+      .update({ first_name: `SP ${assignmentManagerDbUser.first_name}` })
+      .where({ email: member.email });
+
+    logger.info("Updated user with prefix SP", assignmentManagerDbUser);
+  }
+};
+
+main()
+  .then(console.log)
+  .catch(console.error);
