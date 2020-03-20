0c7a377f77261029498996eb037b48cacd75de4c
ben <ben.paul.ryan.packer@gmail.com>
2020-03-20 10:54:23 -0400

    Update 20200310222822_clean-up-indexes.js

--- migrations/20200310222822_clean-up-indexes.js
+++ migrations/20200310222822_clean-up-indexes.js
@@ -1,11 +1,9 @@
 exports.up = function(knex) {
   return knex.schema.raw(`
-    create index campaign_contact_partial_assignment_id_index (assignment_id) where archived = false;
     create unique index campaign_contact_cell_campaign_id_unique (cell, campaign_id);
 
     drop index campaign_contact_campaign_id_cell_unique;
     drop index campaign_contact_timezone_index;
-    drop index campaign_contact_assignment_id_index;
     drop index campaign_contact_cell_index;
 
     drop index message_assignment_id_foreign;
@@ -15,12 +13,10 @@ exports.up = function(knex) {
 
 exports.down = function(knex) {
   return knex.schema.raw(`
-    drop index campaign_contact_partial_assignment_id_index (assignment_id) where archived = false;
     drop index campaign_contact_cell_campaign_id_unique (cell, campaign_id);
 
     create index campaign_contact_campaign_id_cell_unique on campaign_contact (campaign_id, cell);
     create index campaign_contact_timezone_index on campaign_contact (timezone);
-    create index campaign_contact_assignment_id_index on campaign_contact (assignment_id);
     create index campaign_contact_cell_index on campaign_contact (cell);
 
     create index message_assignment_id_foreign on message (assigment_id);
