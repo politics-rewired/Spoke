8c80cb2997f8222572c0551c8998862d00687d35
Benjamin Chrobot <benjamin.chrobot@alum.mit.edu>
2020-03-23 14:19:22 -0400

    fix: fix index cleanup migration (#607)
    This fixes up/down behavior on a clean database.

--- migrations/20200310222822_clean-up-indexes.js
+++ migrations/20200310222822_clean-up-indexes.js
@@ -1,29 +1,37 @@
 exports.up = function(knex) {
   return knex.schema.raw(`
-    create unique index campaign_contact_cell_campaign_id_unique on campaign_contact (cell, campaign_id);
+    -- Swap order of cell, campaign_id in unique constraint
+    alter table campaign_contact add constraint campaign_contact_cell_campaign_id_unique unique (cell, campaign_id);
+    alter table campaign_contact drop constraint campaign_contact_campaign_id_cell_unique;
+
+    -- Add partial index on archived = false for performance
     create index campaign_contact_partial_assignment_id_index on campaign_contact (assignment_id) where archived = false;
 
-    drop index campaign_contact_campaign_id_cell_unique;
+    -- Drop unused indexes that just slow down vacuuming
     drop index campaign_contact_timezone_index;
     drop index campaign_contact_assignment_id_index;
     drop index campaign_contact_cell_index;
 
-    drop index message_assignment_id_foreign;
+    -- Drop this unused relationship in favor of campaign contact <> assignment
     alter table message drop constraint message_assignment_id_foreign;
   `);
 };
 
 exports.down = function(knex) {
   return knex.schema.raw(`
-    drop index campaign_contact_cell_campaign_id_unique;
-    drop index campaign_contact_partial_assignment_id_index;
+    -- Restore message <> assignment relationship
+    alter table message add constraint message_assignment_id_foreign foreign key (assignment_id) references assignment (id);
 
-    create index campaign_contact_campaign_id_cell_unique on campaign_contact (campaign_id, cell);
-    create index campaign_contact_timezone_index on campaign_contact (timezone);
+    -- Restore indexes
     create index campaign_contact_cell_index on campaign_contact (cell);
     create index campaign_contact_assignment_id_index on campaign_contact (assignment_id);
+    create index campaign_contact_timezone_index on campaign_contact (timezone);
 
-    create index message_assignment_id_foreign on message (assigment_id);
-    alter table message add constraint message_assignment_id_foreign foreign key (assignment_id) references assignment (id);
+    -- Drop partial index on archived = false
+    drop index campaign_contact_partial_assignment_id_index;
+
+    -- Restore original ordering of unique constraint
+    alter table campaign_contact add constraint campaign_contact_campaign_id_cell_unique unique (campaign_id, cell);
+    alter table campaign_contact drop constraint campaign_contact_cell_campaign_id_unique;
   `);
 };
