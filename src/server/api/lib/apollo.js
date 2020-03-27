2942f93c87b5173afbf46fb3c0cfab9f8d51927d
Benjamin Chrobot <benjamin.chrobot@alum.mit.edu>
2020-03-27 08:00:18 -0400

    chore: add apollo utils

---
+++ src/server/api/lib/apollo.js
@@ -0,0 +1,20 @@
+export const nodeHasQueryPath = (node, path) => {
+  const pathComponents = path.split(".");
+  const searchName = pathComponents.shift();
+
+  if (node.name.value !== searchName) return false;
+
+  if (pathComponents.length === 0) return true;
+
+  // Can we traverse further?
+  if (!node.selectionSet.selections) return false;
+  const { selections } = node.selectionSet;
+  const nextNode = selections.find(s => s.name.value === pathComponents[0]);
+  if (!nextNode) return false;
+
+  const nextPath = pathComponents.join(".");
+  return nodeHasQueryPath(nextNode, nextPath);
+};
+
+export const infoHasQueryPath = (info, path, fieldIndex = 0) =>
+  nodeHasQueryPath(info.fieldNodes[fieldIndex], path);
