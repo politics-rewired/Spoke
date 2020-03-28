6fff3a09c4c7119531c41fae479846c5b1b21c4f
Benjamin Chrobot <benjamin.chrobot@alum.mit.edu>
2020-03-28 08:53:12 -0400

    refactor: use shorthand array generation

--- src/containers/Settings/components/General.jsx
+++ src/containers/Settings/components/General.jsx
@@ -95,34 +95,7 @@ class Settings extends React.Component {
       textingHoursEnd: yup.number().required()
     });
 
-    const hours = [
-      0,
-      1,
-      2,
-      3,
-      4,
-      5,
-      6,
-      7,
-      8,
-      9,
-      10,
-      11,
-      12,
-      13,
-      14,
-      15,
-      16,
-      17,
-      18,
-      19,
-      20,
-      21,
-      22,
-      23,
-      24
-    ];
-    const hourChoices = hours.map(hour => ({
+    const hourChoices = [...Array(25).keys()].map(hour => ({
       value: hour,
       label: formatTextingHours(hour)
     }));
