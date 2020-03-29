975d94b359ab268f199e335e88d2da11916fd91c
Benjamin Chrobot <benjamin.chrobot@alum.mit.edu>
2020-03-28 20:09:44 -0400

    build: add typescript support

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-  presets: ["@babel/preset-react", "@babel/preset-env"],
+  presets: [
+    "@babel/preset-react",
+    "@babel/preset-env",
+    "@babel/preset-typescript"
+  ],
   plugins: [
     "react-hot-loader/babel",
     ["@babel/plugin-transform-runtime", { corejs: 3 }],
