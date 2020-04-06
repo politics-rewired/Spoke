4e0f4b6b8c5f40d9cf7b0870e4eb1f5559767b0f
Benjamin Chrobot <benjamin.chrobot@alum.mit.edu>
2020-04-06 18:33:23 -0400

    build(webpack): resolve typescript files

--- webpack.config.js
+++ webpack.config.js
@@ -35,7 +35,7 @@ module.exports = {
     bundle: "./client/index.jsx"
   },
   resolve: {
-    extensions: [".js", ".jsx", ".json"]
+    extensions: [".ts", ".tsx", ".js", ".jsx", ".json"]
   },
   module: {
     rules: [
