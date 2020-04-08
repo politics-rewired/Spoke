cd4a5e59ad382cc66f80f5882a213abf18b364c4
Benjamin Chrobot <benjamin.chrobot@alum.mit.edu>
2020-04-07 20:56:24 -0400

    chore: add deep difference util method

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,2 +1,26 @@
+import transform from "lodash/transform";
+import isEqual from "lodash/isEqual";
+import isObject from "lodash/isObject";
+
 export const sleep = (ms = 0) =>
   new Promise(resolve => setTimeout(resolve, ms));
+
+/**
+ * Deep diff between two object, using lodash
+ * @param  {Object} object Object compared
+ * @param  {Object} base   Object to compare with
+ * @return {Object}        Return a new object who represent the diff
+ */
+export function difference(object, base) {
+  function changes(object, base) {
+    return transform(object, function(result, value, key) {
+      if (!isEqual(value, base[key])) {
+        result[key] =
+          isObject(value) && isObject(base[key])
+            ? changes(value, base[key])
+            : value;
+      }
+    });
+  }
+  return changes(object, base);
+}
