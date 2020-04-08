02b713aba9014f33036a8e4d0047d2839def66ed
Benjamin Chrobot <benjamin.chrobot@alum.mit.edu>
2020-04-07 20:55:24 -0400

    refactor: clean up date picker props passing

--- src/components/forms/GSDateField.jsx
+++ src/components/forms/GSDateField.jsx
@@ -5,30 +5,33 @@ import GSFormField from "./GSFormField";
 
 export default class GCDateField extends GSFormField {
   render() {
-    const momentDate = moment(this.props.value);
-    let value = {};
+    const {
+      value: momentVal,
+      type,
+      utcOffset,
+      errorText,
+      onChange,
+      ...childProps
+    } = this.props;
+
+    const momentDate = moment(momentVal);
     let oldDate = null;
     if (momentDate.isValid()) {
       const fakeDate = momentDate
-        .add(this.props.utcOffset - moment().utcOffset(), "minutes")
+        .add(utcOffset - moment().utcOffset(), "minutes")
         .toDate();
       oldDate = moment(fakeDate).toObject();
-      value = { value: fakeDate };
+      childProps.value = fakeDate;
     }
-    const propCopy = {
-      ...this.props
-    };
-    delete propCopy.value;
-    delete propCopy.type;
 
     return (
       <DatePicker
-        {...propCopy}
+        {...childProps}
         floatingLabelText={this.floatingLabelText()}
         onChange={(_, date) => {
           let newDate = moment(date);
           if (!newDate.isValid()) {
-            this.props.onChange(null);
+            onChange(null);
           } else {
             newDate = newDate.toObject();
             if (oldDate) {
@@ -37,14 +40,12 @@ export default class GCDateField extends GSFormField {
               newDate.seconds = oldDate.seconds;
             }
             newDate = moment(newDate).add(
-              moment().utcOffset() - this.props.utcOffset,
+              moment().utcOffset() - utcOffset,
               "minutes"
             );
-            this.props.onChange(newDate.toDate());
+            onChange(newDate.toDate());
           }
         }}
-        {...value}
-        errorText={this.props.errorText}
       />
     );
   }
