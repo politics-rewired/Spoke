5ca92193298b0c8f2be18a978f3e2876f1fdc86b
Benjamin Chrobot <benjamin.chrobot@alum.mit.edu>
2020-04-24 09:32:40 -0400

    improvement: pass Spoke's logger to scheduler (#646)

--- src/server/worker.ts
+++ src/server/worker.ts
@@ -1,5 +1,15 @@
-import { makeWorkerUtils, run, Logger } from "graphile-worker";
-import { run as runSchedule } from "graphile-scheduler";
+import {
+  makeWorkerUtils,
+  run,
+  LogFunctionFactory,
+  Logger,
+  Runner,
+  WorkerUtils
+} from "graphile-worker";
+import {
+  run as runSchedule,
+  Runner as ScheduleRunner
+} from "graphile-scheduler";
 
 import { config } from "../config";
 import logger from "../logger";
@@ -7,17 +17,19 @@ import handleAutoassignmentRequest from "./tasks/handle-autoassignment-request";
 import { Pool } from "pg";
 import { releaseStaleReplies } from "./tasks/release-stale-replies";
 
-const logFactory = scope => (level, message, meta) =>
+const logFactory: LogFunctionFactory = scope => (level, message, meta) =>
   logger.log({ level, message, ...meta, ...scope });
 const graphileLogger = new Logger(logFactory);
 
-let runner = undefined;
-let scheduler = undefined;
+let runner: Runner | undefined = undefined;
+let scheduler: ScheduleRunner | undefined = undefined;
 let runnerSemaphore = false;
 
 const workerPool = new Pool({ connectionString: config.DATABASE_URL });
 
-export const getRunner = async (attempt = 0) => {
+export const getRunner = async (
+  attempt = 0
+): Promise<{ runner: Runner; scheduler: ScheduleRunner }> => {
   // We are the first one to request the runner
   if (!runner && !runnerSemaphore) {
     runnerSemaphore = true;
@@ -35,10 +47,13 @@ export const getRunner = async (attempt = 0) => {
 
     scheduler = await runSchedule({
       pgPool: workerPool,
+      // Use <any> cast as short-term workaround for https://github.com/davbeck/graphile-scheduler/pull/2
+      logger: <any>graphileLogger,
       schedules: [
         {
           name: "release-stale-replies",
           pattern: "*/5 * * * *",
+          timeZone: config.TZ,
           task: releaseStaleReplies
         }
       ]
@@ -51,12 +66,12 @@ export const getRunner = async (attempt = 0) => {
     return getRunner(attempt + 1);
   }
 
-  return { runner, scheduler };
+  return { runner: runner!, scheduler: scheduler! };
 };
 
-let worker = undefined;
+let worker: WorkerUtils | undefined = undefined;
 let workerSemaphore = false;
-export const getWorker = async (attempt = 0) => {
+export const getWorker = async (attempt = 0): Promise<WorkerUtils> => {
   // We are the first one to request the worker
   if (!worker && !workerSemaphore) {
     workerSemaphore = true;
@@ -71,5 +86,5 @@ export const getWorker = async (attempt = 0) => {
     return getWorker(attempt + 1);
   }
 
-  return worker;
+  return worker!;
 };
