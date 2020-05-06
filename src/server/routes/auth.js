7f25d115207f23ee8fc9a99e06a3acdc36768cef
Benjamin Chrobot <benjamin.chrobot@alum.mit.edu>
2020-05-06 07:41:54 -0400

    improvement: rate limit requests to superadmin login

--- src/server/routes/auth.js
+++ src/server/routes/auth.js
@@ -1,4 +1,5 @@
 import express from "express";
+import rateLimit from "express-rate-limit";
 const router = express.Router();
 
 import { r } from "../models";
@@ -12,39 +13,49 @@ router.get("/logout-callback", (req, res) => {
   res.redirect("/");
 });
 
-router.post("/superadmin-login", async (req, res, next) => {
-  const secret = config.SUPERADMIN_LOGIN_SECRET;
-  const reqToken = req.get("X-Spoke-Superadmin-Token");
-
-  if (!secret || secret !== reqToken)
-    return res.status(403).send({
-      message:
-        "Either superadmin login is not enabled or you are using an invalid secret"
-    });
-
-  const userQuery = r.knex
-    .select("user.*")
-    .from("user")
-    .join("user_organization", "user_organization.user_id", "user.id")
-    .where({ role: "OWNER" })
-    .first();
-
-  if (req.body.organizationId) {
-    userQuery.where({ organization_id: req.body.organizationId });
-  }
-
-  const user = await userQuery;
-
-  if (user) {
-    const passportUser = sessionUserMap[config.PASSPORT_STRATEGY](user);
-    return req.login(passportUser, err => {
-      if (err) return next(err);
-
-      return res.status(200).send({ message: "Success" });
-    });
-  }
-
-  return res.status(400).send({ message: "No owners" });
+const superadminLoginLimiter = rateLimit({
+  windowMs: 60 * 60 * 1000, // 1 hour window
+  max: 5,
+  skipSuccessfulRequests: true,
+  message: "Too many superadmin login attempts, please try again after an hour"
 });
+router.post(
+  "/superadmin-login",
+  superadminLoginLimiter,
+  async (req, res, next) => {
+    const secret = config.SUPERADMIN_LOGIN_SECRET;
+    const reqToken = req.get("X-Spoke-Superadmin-Token");
+
+    if (!secret || secret !== reqToken)
+      return res.status(403).send({
+        message:
+          "Either superadmin login is not enabled or you are using an invalid secret"
+      });
+
+    const userQuery = r.knex
+      .select("user.*")
+      .from("user")
+      .join("user_organization", "user_organization.user_id", "user.id")
+      .where({ role: "OWNER" })
+      .first();
+
+    if (req.body.organizationId) {
+      userQuery.where({ organization_id: req.body.organizationId });
+    }
+
+    const user = await userQuery;
+
+    if (user) {
+      const passportUser = sessionUserMap[config.PASSPORT_STRATEGY](user);
+      return req.login(passportUser, err => {
+        if (err) return next(err);
+
+        return res.status(200).send({ message: "Success" });
+      });
+    }
+
+    return res.status(400).send({ message: "No owners" });
+  }
+);
 
 export default router;
