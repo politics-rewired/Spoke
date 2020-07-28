f3bdd366c87e23b6f9d57dc740540b0438b40e05
ben <ben.paul.ryan.packer@gmail.com>
2020-07-28 18:46:09 -0400

    feat: better auto vacuum parameters (#698)

---
+++ migrations/20200727165444_better-autovacuum-parameters.js
@@ -0,0 +1,25 @@
+exports.up = function(knex) {
+  return knex.schema.raw(`
+    alter table campaign_contact set (autovacuum_vacuum_scale_factor = 0, autovacuum_vacuum_threshold = 20000);
+    alter table message set (autovacuum_vacuum_scale_factor = 0, autovacuum_vacuum_threshold = 20000);
+    alter table messaging_service_stick set (autovacuum_vacuum_scale_factor = 0, autovacuum_vacuum_threshold = 20000);
+    alter table assignment_request set (autovacuum_vacuum_scale_factor = 0, autovacuum_vacuum_threshold = 5000);
+    alter table all_question_response set (autovacuum_vacuum_scale_factor = 0, autovacuum_vacuum_threshold = 2000);
+
+    alter table public.campaign_contact set (fillfactor = 50);
+    alter table public.message set (fillfactor = 50);
+  `);
+};
+
+exports.down = function(knex) {
+  return knex.schema.raw(`
+    alter table campaign_contact set (autovacuum_vacuum_scale_factor = 0.2, autovacuum_vacuum_threshold = 50);
+    alter table message set (autovacuum_vacuum_scale_factor = 0.2, autovacuum_vacuum_threshold = 50);
+    alter table messaging_service_stick set (autovacuum_vacuum_scale_factor = 0.2, autovacuum_vacuum_threshold = 50);
+    alter table assignment_request set (autovacuum_vacuum_scale_factor = 0.2, autovacuum_vacuum_threshold = 50);
+    alter table all_question_response set (autovacuum_vacuum_scale_factor = 0.2, autovacuum_vacuum_threshold = 50);
+
+    alter table public.campaign_contact set (fillfactor = 100);
+    alter table public.message set (fillfactor = 100);
+  `);
+};
