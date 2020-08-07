aab32c07c0dced9a0e9f182bdf879adf1ae6c283
ben <ben.paul.ryan.packer@gmail.com>
2020-08-07 07:13:09 -0400

    feat: deprioritize handling delivery reports (#702)

---
+++ migrations/20200804190149_deprioritize-handle-delivery-reports.js
@@ -0,0 +1,30 @@
+exports.up = function(knex) {
+  return knex.schema.raw(`
+    create or replace function public.tg__log__handle_delivery_report() returns trigger as $$
+    begin
+      perform graphile_worker.add_job(
+        'handle-delivery-report',
+        row_to_json(NEW),
+        priority := 5
+      );
+
+      return NEW;
+    end;
+    $$ language plpgsql;
+  `);
+};
+
+exports.down = function(knex) {
+  return knex.schema.raw(`
+    create or replace function public.tg__log__handle_delivery_report() returns trigger as $$
+    begin
+      perform graphile_worker.add_job(
+        'handle-delivery-report',
+        row_to_json(NEW)
+      );
+
+      return NEW;
+    end;
+    $$ language plpgsql;
+  `);
+};
