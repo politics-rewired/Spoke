fa16117a3f216b3c705b8709089647e5bd1e6632
ben-pr-p <ben.paul.ryan.packer@gmail.com>
2020-09-03 15:00:01 -0400

    fix(assignable_campaigns): only exclude overdue campaigns from initials

--- migrations/20200903111251_exclude-overdue-campaigns-from-autoassignment.js
+++ migrations/20200903111251_exclude-overdue-campaigns-from-autoassignment.js
@@ -1,27 +1,35 @@
 
 exports.up = function(knex) {
   return knex.schema.raw(`
-    create or replace view assignable_campaigns as (
-      select id, title, organization_id, limit_assignment_to_teams
-      from campaign
-      where is_started = true
-        and is_archived = false
-        and is_autoassign_enabled = true
-        and texting_hours_end > extract(hour from (CURRENT_TIMESTAMP at time zone campaign.timezone))
-        and now() < date_trunc('day', (due_by + interval '24 hours') at time zone campaign.timezone)
+    create view assignable_campaigns_with_needs_message as (
+      select *
+      from assignable_campaigns
+      where
+        exists (
+          select 1
+          from assignable_needs_message
+          where campaign_id = assignable_campaigns.id
+        )
+        and not exists (
+          select 1
+          from campaign
+          where campaign.id = assignable_campaigns.id
+            and now() > date_trunc('day', (due_by + interval '24 hours') at time zone campaign.timezone)
+        )
     );
   `)
 };
 
 exports.down = function(knex) {
   return knex.schema.raw(`
-    create or replace view assignable_campaigns as (
-      select id, title, organization_id, limit_assignment_to_teams
-      from campaign
-      where is_started = true
-        and is_archived = false
-        and is_autoassign_enabled = true
-        and texting_hours_end > extract(hour from (CURRENT_TIMESTAMP at time zone campaign.timezone))
+    create view assignable_campaigns_with_needs_message as (
+      select *
+      from assignable_campaigns
+      where exists (
+        select 1
+        from assignable_needs_message
+        where campaign_id = assignable_campaigns.id
+      )
     );
   `)
 };
