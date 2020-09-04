2a97a0063449f3c0ca1e6e908bcd75032d609ed8
ben <ben.paul.ryan.packer@gmail.com>
2020-09-04 09:42:05 -0400

    fix(assignable_campaigns): add due_by clause (#725)
    * fix(assignable_campaigns): add due_by clause
    
    * fix(assignable_campaigns): only exclude overdue campaigns from initials
    
    * fix(assignable_campaigns): use create or replace view

---
+++ migrations/20200903111251_exclude-overdue-campaigns-from-autoassignment.js
@@ -0,0 +1,35 @@
+
+exports.up = function(knex) {
+  return knex.schema.raw(`
+    create or replace view assignable_campaigns_with_needs_message as (
+      select *
+      from assignable_campaigns
+      where
+        exists (
+          select 1
+          from assignable_needs_message
+          where campaign_id = assignable_campaigns.id
+        )
+        and not exists (
+          select 1
+          from campaign
+          where campaign.id = assignable_campaigns.id
+            and now() > date_trunc('day', (due_by + interval '24 hours') at time zone campaign.timezone)
+        )
+    );
+  `)
+};
+
+exports.down = function(knex) {
+  return knex.schema.raw(`
+    create or replace view assignable_campaigns_with_needs_message as (
+      select *
+      from assignable_campaigns
+      where exists (
+        select 1
+        from assignable_needs_message
+        where campaign_id = assignable_campaigns.id
+      )
+    );
+  `)
+};
