f2d38e14495ca1ff8d5bf5237b6f6da079909dac
Benjamin Chrobot <benjamin.chrobot@alum.mit.edu>
2020-09-13 12:10:14 -0400

    perf: add index for releasing unhandled replies

---
+++ migrations/20200913120611_release_replies_index.js
@@ -0,0 +1,16 @@
+exports.up = function(knex) {
+  return knex.schema.raw(`
+    create index campaign_contact_release_unhandled_replies_idx
+      on campaign_contact (campaign_id, updated_at)
+      with (fillfactor = 70)
+      where
+        message_status = 'needsResponse'
+        and assignment_id is not null;
+  `);
+};
+
+exports.down = function(knex) {
+  return knex.schema.raw(`
+    drop index campaign_contact_release_unhandled_replies_idx;
+  `);
+};
