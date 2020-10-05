4f2faf193dbb827da01a48435fbbf97989bfbb72
Benjamin Chrobot <benjamin.chrobot@alum.mit.edu>
2020-10-04 21:58:22 -0400

    fix: fetch user emails on demand

--- src/server/tasks/sync-slack-team-members.ts
+++ src/server/tasks/sync-slack-team-members.ts
@@ -1,15 +1,16 @@
 import { Task, JobHelpers } from "pg-compose";
-import { PoolClient } from "pg";
 import bot from "slack";
 import promiseRetry from "promise-retry";
+import isEmpty from "lodash/isEmpty";
 
 import { config } from "../../config";
 import { sleep } from "../../lib/utils";
+import { withTransaction } from "../utils";
 
 const retrySlack = async <T extends unknown>(
   fn: () => Promise<T>
 ): Promise<T> =>
-  promiseRetry(retry =>
+  promiseRetry({ retries: 5, maxTimeout: 1000 }, retry =>
     fn().catch(err => {
       if (err.message === "ratelimited") {
         const retryS = (err.retry && parseInt(err.retry, 10)) || 0;
@@ -49,8 +50,9 @@ const fetchAllChannels = async (
   const params = {
     ...PARAMS,
     types: "public_channel,private_channel",
-    limit: 1000,
-    ...(next_cursor === "" ? {} : { cursor: next_cursor })
+    exclude_archived: true,
+    limit: 200,
+    ...(isEmpty(next_cursor) ? {} : { cursor: next_cursor })
   };
   const response = await retrySlack(() => bot.conversations.list(params));
   const { channels, response_metadata } = response;
@@ -72,37 +74,6 @@ const fetchAllChannels = async (
   }
 };
 
-interface FetchUserListOptions extends SlackPagination<any> {}
-
-const fetchUserList = async (
-  options: FetchUserListOptions = {}
-): Promise<UserEmailMap> => {
-  const { acc = [], next_cursor } = options;
-  const params = {
-    ...PARAMS,
-    limit: 1000,
-    ...(next_cursor === "" ? {} : { cursor: next_cursor })
-  };
-  const response = await retrySlack(() => bot.users.list(params));
-  const { members, response_metadata } = response;
-  const strippedMembers = members.map(({ id, profile: { email } }) => ({
-    id,
-    email
-  }));
-  if (response_metadata.next_cursor) {
-    return await fetchUserList({
-      acc: acc.concat(strippedMembers),
-      next_cursor: response_metadata.next_cursor
-    });
-  } else {
-    const allMembers = acc.concat(strippedMembers);
-    return allMembers.reduce<UserEmailMap>(
-      (acc, user) => ({ ...acc, [user.id]: user.email }),
-      {}
-    );
-  }
-};
-
 interface FetchChannelMembersOptions extends SlackPagination<any> {
   channelId: string;
 }
@@ -116,7 +87,7 @@ const fetchChannelMembers = async (
     ...PARAMS,
     channel: channelId,
     limit: 1000,
-    ...(next_cursor === "" ? {} : { cursor: next_cursor })
+    ...(isEmpty(next_cursor) ? {} : { cursor: next_cursor })
   };
   const response = await retrySlack(() => bot.conversations.members(params));
   const { members, response_metadata } = response;
@@ -134,38 +105,29 @@ const fetchChannelMembers = async (
   }
 };
 
-type TransactionCallback<T> = (client: PoolClient) => Promise<T>;
-const withTransaction = async <T extends unknown>(
-  client: PoolClient,
-  callback: TransactionCallback<T>
-) => {
-  await client.query("begin");
-  try {
-    const result = await callback(client);
-    await client.query("commit");
-    return result;
-  } catch (err) {
-    await client.query("rollback");
-    throw err;
-  }
-};
+let slackIdEmailCache: { [key: string]: string } = {};
+
+const emailForSlackId = async (slackId: string) =>
+  slackIdEmailCache[slackId]
+    ? slackIdEmailCache[slackId]
+    : retrySlack<string>(() =>
+        bot.users
+          .info({ ...PARAMS, user: slackId })
+          .then(({ user }) => user.profile.email)
+      ).then(email => {
+        slackIdEmailCache[slackId] = email;
+        return email;
+      });
 
 interface SyncTeamOptions {
   spokeTeam: SpokeTeamRow;
   slackChannel: SlackChannelRecord;
   helpers: JobHelpers;
   syncOnEmail: boolean;
-  userEmailMap: { [key: string]: string };
 }
 
 const syncTeam = async (options: SyncTeamOptions) => {
-  const {
-    spokeTeam,
-    slackChannel,
-    helpers,
-    syncOnEmail,
-    userEmailMap
-  } = options;
+  const { spokeTeam, slackChannel, helpers, syncOnEmail } = options;
 
   const allChannelMembers = await fetchChannelMembers({
     channelId: slackChannel.id
@@ -173,53 +135,75 @@ const syncTeam = async (options: SyncTeamOptions) => {
 
   const whereField = syncOnEmail ? "email" : "auth0_id";
   const whereValues = syncOnEmail
-    ? allChannelMembers
-        .map(id => userEmailMap[id])
-        .filter(email => email !== undefined)
+    ? await Promise.all(allChannelMembers.map(emailForSlackId)).then(emails =>
+        emails.filter(email => email !== undefined)
+      )
     : allChannelMembers;
-  const wrappedValues = whereValues.map(str => `'${str}'`).join(", ");
 
-  await helpers.withPgClient(async poolClient =>
+  const updateCount = await helpers.withPgClient(async poolClient =>
     withTransaction(poolClient, async client => {
       await client.query(`delete from user_team where team_id = $1`, [
         spokeTeam.id
       ]);
-      await client.query(
-        `
-          with user_ids_to_add as (
-            select id as user_id
-            from public.user
-            where ${whereField} in (${wrappedValues})
-          ),
-          org_memberships as (
-            insert into user_organization (user_id, organization_id, role)
-            select
-              user_id,
-              $1 as organization_id,
-              'TEXTER' as role
-            from user_ids_to_add
-            on conflict (user_id, organization_id) do nothing
-          )
-          insert into user_team (user_id, team_id)
-          select
-            user_id,
-            $2 as team_id
-          from user_ids_to_add
-        `,
-        [spokeTeam.organization_id, spokeTeam.id]
-      );
+      return client
+        .query<{ count: number }>(
+          `
+            with user_ids_to_add as (
+              select id as user_id
+              from public.user
+              where ${whereField} = any ($3)
+            ),
+            org_memberships as (
+              insert into user_organization (user_id, organization_id, role)
+              select
+                user_id,
+                $1 as organization_id,
+                'TEXTER' as role
+              from user_ids_to_add
+              where not exists (
+                select 1 from user_organization existing
+                where
+                  existing.user_id = user_id
+                  and existing.organization_id = $1
+              )
+              returning user_id
+            ),
+            team_memberships as (
+              insert into user_team (user_id, team_id)
+              select
+                user_id,
+                $2 as team_id
+              from user_ids_to_add
+              where not exists (
+                select 1 from user_team existing
+                where
+                  existing.user_id = user_id
+                  and existing.team_id = $2
+              )
+              returning 1
+            )
+            select count(*) from team_memberships
+          `,
+          [spokeTeam.organization_id, spokeTeam.id, whereValues]
+        )
+        .then(({ rows: [{ count }] }) => count);
     })
   );
+  return updateCount;
 };
 
 export const syncSlackTeamMembers: Task = async (_payload, helpers) => {
   const syncOnEmail = config.PASSPORT_STRATEGY !== "slack";
-  const userEmailMap = syncOnEmail ? await fetchUserList() : {};
+
+  // Reset in-memory cache on each invocation
+  slackIdEmailCache = {};
 
   const { rows: allTeams } = await helpers.query<SpokeTeamRow>(
     `select id, organization_id, title from team`
   );
+
   const allChannels = await fetchAllChannels();
+  const syncedTeams: { title: string; updateCount: number }[] = [];
 
   for (const spokeTeam of allTeams) {
     const normalizedTeamName = spokeTeam.title.toLowerCase().replace(/ /g, "-");
@@ -228,15 +212,18 @@ export const syncSlackTeamMembers: Task = async (_payload, helpers) => {
     });
 
     if (matchingChannel) {
-      await syncTeam({
+      const updateCount = await syncTeam({
         spokeTeam,
         slackChannel: matchingChannel,
         helpers,
-        syncOnEmail,
-        userEmailMap
+        syncOnEmail
       });
+
+      syncedTeams.push({ title: spokeTeam.title, updateCount });
     }
   }
+
+  helpers.logger.info("finished sycing teams", { syncedTeams });
 };
 
 export default syncSlackTeamMembers;
--- src/server/utils.ts
+++ src/server/utils.ts
@@ -1,3 +1,5 @@
+import { PoolClient, Client } from "pg";
+
 /**
  * Convert an Error instance to a plain object, including all its non-iterable properties.
  * @param err Error to convert to Object
@@ -8,3 +10,20 @@ export const errToObj = (err: any): any =>
     acc[name] = err[name];
     return acc;
   }, {});
+
+export type WithClientFn<T> = (client: PoolClient | Client) => Promise<T>;
+
+export const withTransaction = async <T extends unknown>(
+  client: PoolClient | Client,
+  callback: WithClientFn<T>
+) => {
+  await client.query("begin");
+  try {
+    const result = await callback(client);
+    await client.query("commit");
+    return result;
+  } catch (err) {
+    await client.query("rollback");
+    throw err;
+  }
+};
