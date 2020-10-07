aef54d517b81635375848612e630da795b0be7e7
Benjamin Chrobot <benjamin.chrobot@alum.mit.edu>
2020-10-07 10:19:23 -0400

    style: update eslint and prettier config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,23 @@
 module.exports = {
   parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
   plugins: ["@typescript-eslint"],
   extends: [
+    "eslint:recommended",
     "airbnb",
-    "plugin:prettier/recommended",
+    "plugin:react/recommended",
+    "plugin:jsx-a11y/recommended",
     "prettier/react",
     "plugin:@typescript-eslint/eslint-recommended",
-    "plugin:@typescript-eslint/recommended"
+    "plugin:@typescript-eslint/recommended",
+    "prettier/@typescript-eslint",
+    "plugin:prettier/recommended" // Make this the last element so prettier config overrides other formatting rules
   ],
   env: {
     browser: true,
@@ -16,7 +27,13 @@ module.exports = {
     jest: true,
     node: true
   },
+  settings: {
+    react: {
+      version: "detect"
+    }
+  },
   rules: {
+    "prettier/prettier": ["error", {}, { usePrettierrc: true }],
     "jsx-a11y/href-no-hash": ["off"],
     "react/jsx-filename-extension": ["warn", { extensions: [".js", ".jsx"] }],
     "max-len": [
@@ -24,7 +41,7 @@ module.exports = {
       {
         code: 80,
         tabWidth: 2,
-        comments: 80,
+        comments: 120,
         ignoreComments: false,
         ignoreTrailingComments: true,
         ignoreUrls: true,
@@ -33,8 +50,28 @@ module.exports = {
         ignoreRegExpLiterals: true
       }
     ],
+
     // Rewired
+    // --------------------------------
     "comma-dangle": ["error", "never"],
-    semi: ["error", "always"]
-  }
+    semi: ["error", "always"],
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "import/extensions": ["error", "never"],
+
+    // Rules to get linting to pass
+    camelcase: ["off", { properties: "never" }],
+    "no-await-in-loop": ["off"],
+    "@typescript-eslint/no-var-requires": ["off"],
+    "import/order": ["off"],
+    "no-console": ["off"]
+  },
+  overrides: [
+    {
+      // enable the rule specifically for TypeScript files
+      files: ["*.ts", "*.tsx"],
+      rules: {
+        "@typescript-eslint/explicit-module-boundary-types": ["error"]
+      }
+    }
+  ]
 };
