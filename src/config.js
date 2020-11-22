3052a8d5f58297d5b22ff832203337c7cbdbc79b
henryk1229 <henryk1229@gmail.com>
2020-11-22 14:30:28 -0500

    feat: hide shortlink domains behind envvar

--- src/config.js
+++ src/config.js
@@ -314,6 +314,11 @@ const validators = {
       "The interval length in minutes that each troll patrol sweep will examine messages within.",
     default: 6
   }),
+  ENABLE_SHORTLINK_DOMAINS: bool({
+    desc: "Whether to enable shortlink domains",
+    default: false,
+    isClient: true
+  }),
   DISABLE_TEXTER_NOTIFICATIONS: bool({
     desc:
       "Whether to disable texter notifications – if true, should be implemented externally.",
