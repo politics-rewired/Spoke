e41ae55e29c6fd8e9e4e7628c7f11e101e652a4d
Luming Hao <lh00000000@gmail.com>
2020-12-30 23:46:15 -0500

    feat(peopletable): fancy descriptive count messaages

--- src/containers/AdminPeople/PeopleTable.tsx
+++ src/containers/AdminPeople/PeopleTable.tsx
@@ -13,6 +13,8 @@ import InfiniteRelayList from "../../components/InfiniteRelayList";
 import { AdminPeopleContext, PersonMutationHandler } from "./context";
 import PeopleRow from "./PeopleRow";
 
+const PAGE_SIZE = 20;
+
 const query = gql`
   query getPeople(
     $organizationId: String!
@@ -47,11 +49,9 @@ const query = gql`
     }
   }
 `;
-
-const PAGE_SIZE = 20;
 interface PeopleTableProps {
   context: AdminPeopleContext;
-  onlyCampaignId: number | false;
+  onlyCampaignId: string | false;
   nameSearch: string;
   on: PersonMutationHandler;
   freshness: Date;
@@ -67,6 +67,11 @@ const PeopleTable: React.StatelessComponent<PeopleTableProps> = ({
     nameSearch: nameSearch === "" ? undefined : nameSearch,
     ...(onlyCampaignId ? { campaignId: onlyCampaignId } : {})
   };
+
+  const campaignTitle =
+    context.organization.campaigns.campaigns.find(
+      (c) => c.id === onlyCampaignId
+    )?.title || "";
 
   return (
     <Table selectable={false}>
@@ -87,7 +92,32 @@ const PeopleTable: React.StatelessComponent<PeopleTableProps> = ({
           renderNode={(membership: OrganizationMembership) => (
             <PeopleRow membership={membership} context={context} on={on} />
           )}
-          empty={() => <Empty title="No people yet" icon={<PeopleIcon />} />}
+          keyFunc={(membership) => membership.user.id}
+          empty={() => (
+            <Empty
+              title={`Nobody in ${
+                onlyCampaignId ? `campaign ${campaignTitle}` : "organization"
+              } yet`}
+              icon={<PeopleIcon />}
+            />
+          )}
+          cliffHanger={(last) => {
+            const countClause = `${last.pageInfo.totalCount} ${
+              last.pageInfo.totalCount === 1 ? "person" : "people"
+            } found`;
+            const searchClause = nameSearch
+              ? `with search query '${nameSearch}'`
+              : "Try the search (at the top) to filter by name.";
+            const campaignClause = onlyCampaignId
+              ? `in campaign '${campaignTitle}'`
+              : `in organization ${context.organization.id}`;
+
+            return (
+              <div style={{ paddingTop: 8 }}>
+                <span>{`${countClause} ${campaignClause} ${searchClause}.`}</span>
+              </div>
+            );
+          }}
         />
       </TableBody>
     </Table>
