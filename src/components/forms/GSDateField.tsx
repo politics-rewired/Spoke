04e84e990e3b34272f70b2ff24243fe89f877c32
Luming Hao <lh00000000@gmail.com>
2020-12-31 02:03:58 -0500

    style(gsdatefield): remove comments about leftover code

--- src/components/forms/GSDateField.tsx
+++ src/components/forms/GSDateField.tsx
@@ -1,11 +1,4 @@
-// - why `type` cannot be "date"
-// if type={"date"}, React's inputValueTracking.js will kick in,
-//      then Material-ui will automatically nicely format JSDates into 12/21/2020 format,
-//      then inputValueTracking will not understand that format and then block the update,
-//      then the field will revert to having a value of undefined again
-//      then material-ui will automatically format the null value as as the text "mm/dd/yyyy"
-//      which will be colored black and overlap with the desired floatingLabelText() (e.g. "Due By")
-
+/* eslint-disable no-unused-vars */
 import { DateTime } from "luxon";
 import { DatePicker, DatePickerProps } from "material-ui";
 import React from "react";
@@ -44,7 +37,6 @@ export default class GSDateField extends GSFormField<
         locale={this.props.locale}
         className={this.props.className}
         name={this.props.name}
-        // type={this.props.type} // see above
         data-test={this.props["data-test"] as Date}
         floatingLabelText={this.floatingLabelText()}
         onChange={(_, newDate) => {
