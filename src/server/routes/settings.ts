b7b0b671e2c933cbbcf389cdd3674a54a651a909
Benjamin Chrobot <benjamin.chrobot@alum.mit.edu>
2021-02-27 10:34:09 -0500

    feat: source theme values from database

--- src/server/routes/settings.ts
+++ src/server/routes/settings.ts
@@ -2,19 +2,33 @@ import express from "express";
 
 import { clientConfig } from "../../config";
 import { CustomTheme } from "../../styles/types";
+import { r } from "../models";
 
 const router = express.Router();
+
+interface InstanceSettingRow {
+  name: string;
+  type: string;
+  value: string;
+}
 
 router.get("/settings/instance", (req, res) => {
   return res.json(clientConfig);
 });
 
-router.get("/settings/theme", (req, res) => {
+router.get("/settings/theme", async (req, res) => {
+  const settings = await r
+    .knex<InstanceSettingRow>("instance_setting")
+    .where("name", "like", "theme.%");
+
+  const getSetting = (settingName: string) =>
+    settings.find(({ name }) => name === settingName)?.value;
+
   const customTheme: CustomTheme = {
-    primaryColor: "#187DBB",
-    secondaryColor: "#FF0000",
-    logoUrl: "https://politicsrewired.com/images/grritsabear.png",
-    welcomeText: "Grr it's a bear!"
+    primaryColor: getSetting("theme.primaryColor"),
+    secondaryColor: getSetting("theme.secondaryColor"),
+    logoUrl: getSetting("theme.logoUrl"),
+    welcomeText: getSetting("theme.welcomeText")
   };
   return res.json(customTheme);
 });
