623388dee26b72be24bef5a592da78e75bded8da
ben-pr-p <ben.paul.ryan.packer@gmail.com>
2021-09-07 17:16:27 -0500

    chore(resend-message): tabs to spaces

--- src/server/tasks/resend-message.ts
+++ src/server/tasks/resend-message.ts
@@ -8,28 +8,28 @@ import { SendMessagePayload } from "../api/lib/types";
 To create these jobs, run:
 
 select graphile_worker.add_job(
-	'resend-message',
-	row_to_json(jobs)
+  'resend-message',
+  row_to_json(jobs)
 )
 from (
-	select
-	  m.id,
-		m.user_id,
-		m.campaign_contact_id,
-		m.text,
-		m.contact_number,
-		m.assignment_id,
-		m.send_status,
-		m.service,
-		m.is_from_contact,
-		m.queued_at,
-		m.send_before, -- maybe change
-		m.script_version_hash,
-		c.organization_id
-	from message m
-	join campaign_contact cc on cc.id = m.campaign_contact_id
-	join campaign c on cc.campaign_id = c.id
-	where -- your custom requeue where clauses here
+  select
+    m.id,
+    m.user_id,
+    m.campaign_contact_id,
+    m.text,
+    m.contact_number,
+    m.assignment_id,
+    m.send_status,
+    m.service,
+    m.is_from_contact,
+    m.queued_at,
+    m.send_before, -- maybe change
+    m.script_version_hash,
+    c.organization_id
+  from message m
+  join campaign_contact cc on cc.id = m.campaign_contact_id
+  join campaign c on cc.campaign_id = c.id
+  where -- your custom requeue where clauses here
 ) jobs
 
 */
