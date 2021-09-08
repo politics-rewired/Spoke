c90cade255e214e42b44267a19b7020194c97c94
Benjamin Chrobot <benjamin.chrobot@alum.mit.edu>
2021-09-08 16:32:55 -0400

    chore: update copy

--- src/components/NotificationCard.tsx
+++ src/components/NotificationCard.tsx
@@ -36,11 +36,13 @@ const Register10DlcBrandNoticeCard: React.FC<Register10DlcBrandNotice> = (
       <CardContent>
         <p>
           You must provide details required for us to register a 10DLC brand on
-          your behalf by September 24th!
+          your behalf by September 24th! If you do not provide this information
+          by then, you may not be able to send messages starting on October 1st
+          until 3-5 business days after you provide this information.
         </p>
 
         <p>
-          To learn more about this change please see our{" "}
+          To learn more about this change, please see our{" "}
           <a
             href="https://docs.spokerewired.com/article/124-10dlc"
             target="_blank"
