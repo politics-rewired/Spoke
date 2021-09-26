0d4e3c4fd3fad13165e561dac864e40d01431670
Benjamin Chrobot <benjamin.chrobot@alum.mit.edu>
2021-09-26 11:08:26 -0400

    fix: use updated portal schema

--- src/server/lib/notices/register-10dlc-brand.ts
+++ src/server/lib/notices/register-10dlc-brand.ts
@@ -5,22 +5,22 @@ import { r } from "../../models";
 import { OrgLevelNotificationGetter } from "./types";
 
 const graphqlQuery = `
-  query AnonGetTcr10DlcBrand($switchboardProfileId: String!) {
+  query AnonGetTcr10DlcSurvey($switchboardProfileId: String!) {
     billingAccountId: billingAccountIdBySwitchboardProfileId(
       switchboardProfileId: $switchboardProfileId
     )
     billingAccountName: billingAccountNameBySwitchboardProfileId(
       switchboardProfileId: $switchboardProfileId
     )
-    brand: tcr10DlcBrandBySwitchboardProfileId(
+    survey: tcr10DlcSurveyBySwitchboardProfileId(
       switchboardProfileId: $switchboardProfileId
     ) {
-      ...Tcr10DlcBrandInfo
+      ...Tcr10DlcSurveyInfo
       __typename
     }
   }
 
-  fragment Tcr10DlcBrandInfo on Tcr10DlcBrand {
+  fragment Tcr10DlcSurveyInfo on Tcr10DlcSurvey {
     id
     nodeId
     legalCompanyName
@@ -82,7 +82,7 @@ export const get10DlcBrandNotices: OrgLevelNotificationGetter = async (
           .post(`https://api.portal.spokerewired.com/graphql`)
           .send(payload);
 
-        if (!response.body.data.brand) {
+        if (!response.body.data.survey) {
           return {
             __typename: "Register10DlcBrandNotice",
             id: messaging_service_sid,
