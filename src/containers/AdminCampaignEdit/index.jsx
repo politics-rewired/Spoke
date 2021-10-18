467a5fb46362a7ca651c3c418983d8817d8c54ef
Benjamin Chrobot <benjamin.chrobot@alum.mit.edu>
2021-10-18 09:26:43 -0400

    fix: pass campaign id as string

--- src/containers/AdminCampaignEdit/index.jsx
+++ src/containers/AdminCampaignEdit/index.jsx
@@ -736,7 +736,7 @@ class AdminCampaignEdit extends React.Component {
     const sections = this.sections();
     const { expandedSection, requestError } = this.state;
     const { adminPerms, match, theme: stableMuiTheme } = this.props;
-    const campaignId = parseInt(match.params.campaignId, 10);
+    const { campaignId } = match.params;
     const isNew = this.isNew();
     const saveLabel = isNew ? "Save and goto next section" : "Save";
 
