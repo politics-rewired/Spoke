7e13942f4caf785f6764bebc693d7f9714244a2b
Benjamin Chrobot <benjamin.chrobot@alum.mit.edu>
2022-01-26 17:20:13 -0500

    ci: switch to github actions (#1001)
    Reviewed-by: @ben-pr-p

--- src/server/api/lib/campaign.spec.ts
+++ src/server/api/lib/campaign.spec.ts
@@ -1,4 +1,4 @@
-import { Pool, PoolClient } from "pg";
+import { Pool } from "pg";
 import supertest from "supertest";
 
 import { createSession } from "../../../../__test__/lib/session";
@@ -12,126 +12,129 @@ import {
 import { UserRoleType } from "../../../api/organization-membership";
 import { config } from "../../../config";
 import app from "../../app";
+import { withClient } from "../../utils";
 import { OrganizationRecord } from "../types";
 import { getDeliverabilityStats } from "./campaign";
 
 describe("getDeliverabilityStats", () => {
   let pool: Pool;
-  let client: PoolClient;
 
   beforeAll(async () => {
     pool = new Pool({ connectionString: config.TEST_DATABASE_URL });
-    client = await pool.connect();
   });
 
   afterAll(async () => {
-    if (client) client.release();
     if (pool) await pool.end();
   });
 
   test("generates correct stats for mixed errors", async () => {
-    const {
-      campaign,
-      contacts: [contact],
-      assignments: [assignment]
-    } = await createCompleteCampaign(client, { texters: 1, contacts: 1 });
-    const message = {
-      campaignContactId: contact.id,
-      assignmentId: assignment.id,
-      sendStatus: "ERROR"
-    };
+    await withClient(pool, async (client) => {
+      const {
+        campaign,
+        contacts: [contact],
+        assignments: [assignment]
+      } = await createCompleteCampaign(client, { texters: 1, contacts: 1 });
+      const message = {
+        campaignContactId: contact.id,
+        assignmentId: assignment.id,
+        sendStatus: "ERROR"
+      };
 
-    const createErrorMessage = async (count: number, errorCode?: string) =>
-      Promise.all(
-        [...Array(count)].map((_) =>
-          createMessage(client, {
-            ...message,
-            errorCodes: errorCode ? [errorCode] : undefined
-          })
-        )
-      );
+      const createErrorMessage = async (count: number, errorCode?: string) =>
+        Promise.all(
+          [...Array(count)].map((_) =>
+            createMessage(client, {
+              ...message,
+              errorCodes: errorCode ? [errorCode] : undefined
+            })
+          )
+        );
 
-    const errors: [number, string | null][] = [
-      [4, "30001"],
-      [3, "30004"],
-      [2, "30011"],
-      [3, null]
-    ];
+      const errors: [number, string | null][] = [
+        [4, "30001"],
+        [3, "30004"],
+        [2, "30011"],
+        [3, null]
+      ];
 
-    for (const [count, errorCode] of errors) {
-      await createErrorMessage(count, errorCode ?? undefined);
-    }
+      for (const [count, errorCode] of errors) {
+        await createErrorMessage(count, errorCode ?? undefined);
+      }
 
-    const stats = await getDeliverabilityStats(campaign.id);
-    expect(stats.deliveredCount).toBe(0);
-    expect(stats.sentCount).toBe(0);
-    expect(stats.errorCount).toBe(12);
-    expect(stats.specificErrors).toHaveLength(4);
+      const stats = await getDeliverabilityStats(campaign.id);
+      expect(stats.deliveredCount).toBe(0);
+      expect(stats.sentCount).toBe(0);
+      expect(stats.errorCount).toBe(12);
+      expect(stats.specificErrors).toHaveLength(4);
 
-    errors.forEach(([count, errorCode]) => {
-      // See: https://medium.com/@andrei.pfeiffer/jest-matching-objects-in-array-50fe2f4d6b98
-      expect(stats.specificErrors).toEqual(
-        expect.arrayContaining([expect.objectContaining({ count, errorCode })])
-      );
+      errors.forEach(([count, errorCode]) => {
+        // See: https://medium.com/@andrei.pfeiffer/jest-matching-objects-in-array-50fe2f4d6b98
+        expect(stats.specificErrors).toEqual(
+          expect.arrayContaining([
+            expect.objectContaining({ count, errorCode })
+          ])
+        );
+      });
     });
   });
 
   test("generates correct stats for mixed null and empty list error codes", async () => {
-    const {
-      campaign,
-      contacts: [contact],
-      assignments: [assignment]
-    } = await createCompleteCampaign(client, { texters: 1, contacts: 1 });
+    await withClient(pool, async (client) => {
+      const {
+        campaign,
+        contacts: [contact],
+        assignments: [assignment]
+      } = await createCompleteCampaign(client, { texters: 1, contacts: 1 });
 
-    await createMessage(client, {
-      campaignContactId: contact.id,
-      assignmentId: assignment.id,
-      sendStatus: "ERROR",
-      errorCodes: []
+      await createMessage(client, {
+        campaignContactId: contact.id,
+        assignmentId: assignment.id,
+        sendStatus: "ERROR",
+        errorCodes: []
+      });
+
+      await createMessage(client, {
+        campaignContactId: contact.id,
+        assignmentId: assignment.id,
+        sendStatus: "ERROR",
+        errorCodes: undefined
+      });
+
+      const stats = await getDeliverabilityStats(campaign.id);
+
+      expect(stats.deliveredCount).toBe(0);
+      expect(stats.sentCount).toBe(0);
+      expect(stats.errorCount).toBe(2);
+      expect(stats.specificErrors).toHaveLength(1);
+      expect(stats.specificErrors[0].errorCode).toBeNull();
     });
-
-    await createMessage(client, {
-      campaignContactId: contact.id,
-      assignmentId: assignment.id,
-      sendStatus: "ERROR",
-      errorCodes: undefined
-    });
-
-    const stats = await getDeliverabilityStats(campaign.id);
-
-    expect(stats.deliveredCount).toBe(0);
-    expect(stats.sentCount).toBe(0);
-    expect(stats.errorCount).toBe(2);
-    expect(stats.specificErrors).toHaveLength(1);
-    expect(stats.specificErrors[0].errorCode).toBeNull();
   });
 });
 
 describe("create / edit campaign", () => {
   let pool: Pool;
-  let client: PoolClient;
   let organization: OrganizationRecord;
   let agent: supertest.SuperAgentTest;
   let cookies: Record<string, string>;
 
   beforeAll(async () => {
     pool = new Pool({ connectionString: config.TEST_DATABASE_URL });
-    client = await pool.connect();
     agent = supertest.agent(app);
-    organization = await createOrganization(client, {});
+    await withClient(pool, async (client) => {
+      organization = await createOrganization(client, {});
 
-    const password = "KeepItSecretKeepItSafe";
-    const user = await createUser(client, { password });
-    await createUserOrganization(client, {
-      userId: user.id,
-      organizationId: organization.id,
-      role: UserRoleType.OWNER
+      const password = "KeepItSecretKeepItSafe";
+      const user = await createUser(client, { password });
+      await createUserOrganization(client, {
+        userId: user.id,
+        organizationId: organization.id,
+        role: UserRoleType.OWNER
+      });
+      cookies = await createSession({ agent, email: user.email, password });
     });
-    cookies = await createSession({ agent, email: user.email, password });
   });
 
   afterAll(async () => {
-    if (client) client.release();
     if (pool) await pool.end();
   });
 
