e4e0fcd4886fdcbc1eefadb1625cd54417abfc60
Benjamin Chrobot <benjamin.chrobot@alum.mit.edu>
2022-02-16 17:38:05 -0500

    perf: limit contact field selection (#1078)
    reviewed-by: @ben-pr-p

---
+++ src/server/lib/graphql.ts
@@ -0,0 +1,20 @@
+/* eslint-disable import/prefer-default-export */
+import { FieldNode, SelectionNode } from "graphql";
+
+export const selectionsByType = (node: FieldNode) => {
+  const selections = node.selectionSet?.selections ?? [];
+  return selections.reduce<{
+    simple: SelectionNode[];
+    complex: SelectionNode[];
+  }>(
+    (acc, selection) => {
+      if ((selection as FieldNode).selectionSet !== undefined) {
+        acc.complex.push(selection);
+      } else {
+        acc.simple.push(selection);
+      }
+      return acc;
+    },
+    { simple: [], complex: [] }
+  );
+};
