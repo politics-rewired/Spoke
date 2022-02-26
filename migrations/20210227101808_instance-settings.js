d542dd5dec2317c80567c7bc3ea4ff8b4c8b05b0
Benjamin Chrobot <benjamin.chrobot@alum.mit.edu>
2022-02-26 06:06:25 -0500

    style: name all migration functions

--- migrations/20210227101808_instance-settings.js
+++ migrations/20210227101808_instance-settings.js
@@ -1,4 +1,4 @@
-exports.up = function (knex) {
+exports.up = function up(knex) {
   return knex.schema.raw(`
     create table instance_setting (
       name text primary key,
@@ -8,7 +8,7 @@ exports.up = function (knex) {
  `);
 };
 
-exports.down = function (knex) {
+exports.down = function down(knex) {
   return knex.schema.raw(`
     drop table table instance_setting;
  `);
