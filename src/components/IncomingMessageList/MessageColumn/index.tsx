476ba05855bdd8698f4d42f64e1a66251b177e0a
Benjamin Chrobot <benjamin.chrobot@alum.mit.edu>
2022-04-08 10:48:42 -0700

    feat: add canned responses to message review

--- src/components/IncomingMessageList/MessageColumn/index.tsx
+++ src/components/IncomingMessageList/MessageColumn/index.tsx
@@ -1,9 +1,12 @@
+import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
 import {
   ConversationInfoFragment,
   ConversationMessageFragment
 } from "@spoke/spoke-codegen";
 import isNil from "lodash/isNil";
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
+import CannedResponseMenu from "src/components/CannedResponseMenu";
 
 import MessageList from "./MessageList";
 import MessageOptOut from "./MessageOptOut";
@@ -17,6 +20,8 @@ const styles: Record<string, React.CSSProperties> = {
   }
 };
 
+type ClickButtonHandler = React.MouseEventHandler<HTMLButtonElement>;
+
 interface Props {
   organizationId: string;
   conversation: ConversationInfoFragment;
@@ -26,6 +31,8 @@ const MessageColumn: React.FC<Props> = (props) => {
   const { organizationId, conversation } = props;
   const { contact } = conversation;
 
+  const [messageText, setMessageText] = useState("");
+  const [anchorEl, setAnchorEl] = useState<Element | null>(null);
   const [isOptedOut, setIsOptedOut] = useState(
     !isNil(conversation.contact.optOut?.cell)
   );
@@ -36,22 +43,62 @@ const MessageColumn: React.FC<Props> = (props) => {
     setMessages(conversation.contact.messages);
   }, [setMessages]);
 
+  const handleOpenCannedResponse: ClickButtonHandler = useCallback(
+    (event) => {
+      setAnchorEl(event.currentTarget);
+    },
+    [setAnchorEl]
+  );
+
+  const handleScriptSelected = useCallback(
+    (script: string) => {
+      setAnchorEl(null);
+      setMessageText(script);
+    },
+    [setAnchorEl]
+  );
+
+  const handleRequestClose = useCallback(() => setAnchorEl(null), [
+    setAnchorEl
+  ]);
+
   return (
-    <div style={styles.container}>
-      <h4>Messages</h4>
-      <MessageList messages={messages} organizationId={organizationId} />
-      {!isOptedOut && (
-        <MessageResponse
-          conversation={conversation}
-          messagesChanged={setMessages}
-        />
-      )}
-      <MessageOptOut
-        contact={contact}
-        isOptedOut={isOptedOut}
-        optOutChanged={setIsOptedOut}
+    <>
+      <div style={styles.container}>
+        <h4>Messages</h4>
+        <MessageList messages={messages} organizationId={organizationId} />
+        {!isOptedOut && (
+          <MessageResponse
+            value={messageText}
+            conversation={conversation}
+            messagesChanged={setMessages}
+            onChange={setMessageText}
+          />
+        )}
+        <Grid container spacing={2} justify="flex-end">
+          <Grid item>
+            <MessageOptOut
+              contact={contact}
+              isOptedOut={isOptedOut}
+              optOutChanged={setIsOptedOut}
+            />
+          </Grid>
+          {!isOptedOut && (
+            <Grid item>
+              <Button variant="contained" onClick={handleOpenCannedResponse}>
+                Canned Responses
+              </Button>
+            </Grid>
+          )}
+        </Grid>
+      </div>
+      <CannedResponseMenu
+        campaignId={conversation.campaign.id ?? undefined}
+        anchorEl={anchorEl ?? undefined}
+        onSelectCannedResponse={handleScriptSelected}
+        onRequestClose={handleRequestClose}
       />
-    </div>
+    </>
   );
 };
 
