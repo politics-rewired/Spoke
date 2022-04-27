d5c23c1ac342ce668cf7479886abc987470a841b
Benjamin Chrobot <benjamin.chrobot@alum.mit.edu>
2022-04-27 11:26:49 -0400

    test: fix test env database url

--- src/server/db.ts
+++ src/server/db.ts
@@ -3,7 +3,9 @@ import { Pool } from "pg";
 import { config } from "../config";
 
 const poolConfig = {
-  connectionString: config.DATABASE_URL,
+  connectionString: config.isTest
+    ? config.TEST_DATABASE_URL
+    : config.DATABASE_URL,
   max: config.WORKER_MAX_POOL
 };
 
