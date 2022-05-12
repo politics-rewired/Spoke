88106681c9cc3d073029578cc7ca30b978170836
ben <ben.paul.ryan.packer@gmail.com>
2022-05-12 09:28:53 -0400

    fix(queue-autosend-initials): autosend assigned messages too

--- src/server/tasks/queue-autosend-initials.ts
+++ src/server/tasks/queue-autosend-initials.ts
@@ -25,7 +25,6 @@ const queueAutoSendInitials: Task = async (payload: Payload, helpers) => {
           -- contact requirements
           and cc.archived = false
           and cc.message_status = 'needsMessage'
-          and cc.assignment_id is null
           -- campaign requirements for autosending
           and c.is_archived = false
           and c.is_started = true
@@ -50,7 +49,7 @@ const queueAutoSendInitials: Task = async (payload: Payload, helpers) => {
           --    and graphile_worker.jobs.key = cc.id::text
           -- )
         -- ordering by campaign id and cell should be fastest since theres a compound key on them
-        order by cc.campaign_id, cc.cell asc
+        order by assignment_id nulls last, cc.campaign_id, cc.cell asc
         limit $1
       ),
       assignments_upserted as (
