c47feafb0eb13f24fb809ee1ad7b1e68d3d0417d
Benjamin Chrobot <benjamin.chrobot@alum.mit.edu>
2022-05-16 15:37:20 -0400

    fix: rewrite campaign preview (#1173)
    Reviewed-by: @ajohn25 
    Reviewed-by: @hiemanshu

---
+++ src/server/routes/campaign-preview.ts
@@ -0,0 +1,49 @@
+import express from "express";
+
+import { symmetricDecrypt } from "../api/lib/crypto";
+import {
+  CampaignRecord,
+  CannedResponseRecord,
+  InteractionStepRecord
+} from "../api/types";
+import renderCampaignPreview from "../lib/templates/pages/campaign-preview";
+import { r } from "../models";
+
+const router = express.Router();
+
+router.get("/preview/:campaignId", async (req, res) => {
+  const token = req.params.campaignId;
+
+  let campaignId;
+  try {
+    campaignId = symmetricDecrypt(token);
+  } catch {
+    return res.status(400).send("bad token");
+  }
+
+  const campaign = await r
+    .reader<CampaignRecord>("campaign")
+    .where({ id: campaignId })
+    .first("*");
+
+  const interactionSteps = await r
+    .reader<InteractionStepRecord>("interaction_step")
+    .where({
+      campaign_id: campaignId,
+      is_deleted: false
+    });
+
+  const cannedResponses = await r
+    .reader<CannedResponseRecord>("canned_response")
+    .where({ campaign_id: campaignId });
+
+  const page = await renderCampaignPreview({
+    campaign,
+    interactionSteps,
+    cannedResponses
+  });
+
+  return res.send(page);
+});
+
+export default router;
