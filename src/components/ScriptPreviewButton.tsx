7cd880cbec99530b1d50b9d6518454a23bd777b8
Aashish John <76596635+ajohn25@users.noreply.github.com>
2022-06-09 00:17:32 -0400

    refactor: extract script preview button (#1259)
    Reviewed-by: @bchrobot
    Reviewed-by: @hiemanshu

---
+++ src/components/ScriptPreviewButton.tsx
@@ -0,0 +1,45 @@
+import Button from "@material-ui/core/Button";
+import Tooltip from "@material-ui/core/Tooltip";
+import { useGetCampaignScriptPreviewQuery } from "@spoke/spoke-codegen";
+import React from "react";
+
+import { useSpokeContext } from "../client/spoke-context";
+import { useAuthzContext } from "./AuthzProvider";
+
+export interface ScriptPreviewButtonProps {
+  campaignId: string;
+}
+
+export const ScriptPreviewButton: React.FC<ScriptPreviewButtonProps> = (
+  props
+) => {
+  const { campaignId } = props;
+  const { orgSettings } = useSpokeContext();
+  const { isAdmin } = useAuthzContext();
+
+  const { data } = useGetCampaignScriptPreviewQuery({
+    variables: { campaignId },
+    skip: campaignId === undefined
+  });
+  const previewUrl = data?.campaign?.previewUrl;
+
+  const showScriptPreview =
+    (isAdmin || orgSettings?.scriptPreviewForSupervolunteers) &&
+    previewUrl !== undefined;
+
+  return showScriptPreview ? (
+    <Tooltip title="View an outline of your script" placement="top">
+      <Button
+        key="open-script-preview"
+        variant="contained"
+        onClick={() => {
+          window.open(`/preview/${previewUrl}`, "_blank");
+        }}
+      >
+        Open Script Preview
+      </Button>
+    </Tooltip>
+  ) : null;
+};
+
+export default ScriptPreviewButton;
--- src/containers/AdminIncomingMessageList/components/IncomingMessageList/index.jsx
+++ src/containers/AdminIncomingMessageList/components/IncomingMessageList/index.jsx
@@ -346,7 +346,6 @@ const queries = {
             campaign {
               id
               title
-              previewUrl
             }
           }
         }
