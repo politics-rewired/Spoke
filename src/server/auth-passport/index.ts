6ae2db937d1fc9f2eb20c87391e8b78800dc4461
Aashish John <aashishjohn25@gmail.com>
2022-06-14 23:01:14 -0400

    fix: fix superadmin login

--- src/server/auth-passport/index.ts
+++ src/server/auth-passport/index.ts
@@ -1,14 +1,6 @@
-import type { UserRecord } from "../api/types";
 import { setupAuth0Passport } from "./auth0";
 import { setupLocalAuthPassport } from "./local";
 import { setupSlackPassport } from "./slack";
-
-// Convert a Spoke user record to the type expected by passport.(de)serializeUser
-export const sessionUserMap = {
-  local: (user: UserRecord) => ({ id: user.id }),
-  auth0: (user: UserRecord) => ({ id: user.auth0_id }),
-  slack: (user: UserRecord) => ({ id: user.auth0_id })
-};
 
 export const passportSetups = {
   local: setupLocalAuthPassport,
