e3d4d1857bb15e8536036aca5193fc22e7f1e011
Benjamin Chrobot <benjamin.chrobot@alum.mit.edu>
2022-08-03 09:27:59 -0400

    Revert "fix(clipboard): fix safari paste popup (#1326)" (#1333)
    This reverts commit a9b753cfbb7059074191f07f9826a44e46c463b7.
    
    Reviewed-by: @hiemanshu

--- src/client/lib/index.ts
+++ src/client/lib/index.ts
@@ -1,3 +1,16 @@
-/* eslint-disable import/prefer-default-export */
 export const supportsClipboard = () =>
   navigator?.clipboard?.readText !== undefined;
+
+export const readClipboardText = async () => {
+  if (navigator?.clipboard?.readText) {
+    return navigator.clipboard.readText().catch(() => "");
+  }
+  return Promise.resolve("");
+};
+
+export const writeClipboardText = async (text: string) => {
+  if (navigator?.clipboard?.writeText) {
+    return navigator.clipboard.writeText(text).catch(() => {});
+  }
+  return Promise.resolve();
+};
