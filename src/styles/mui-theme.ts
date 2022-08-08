0114bad5aca44adac4d163b54141a837412553b4
Hiemanshu Sharma <hiemanshu@gmail.com>
2022-08-08 23:01:51 +0530

    feat(texter conversation): update button in line with design doc (#1340)
    * feat(texter conversation): update button in line with design doc

--- src/styles/mui-theme.ts
+++ src/styles/mui-theme.ts
@@ -41,7 +41,7 @@ export const createMuiThemev1 = (theme: Partial<CustomTheme> = {}) =>
       success: { main: theme.successColor || green[500] },
       warning: { main: theme.warningColor || baseTheme.colors.orange },
       error: { main: theme.errorColor || red[200] },
-      info: { main: theme.infoColor || blue[800] },
+      info: { main: theme.infoColor || blue[800], light: blue[100] },
       text: {
         primary: theme.primaryTextColor || blueGrey[800],
         // Do not provide default of grey[50] here -- v0 and v1 behave differently
