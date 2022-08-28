a2eebf87735f707402c90961868255a6a41c152b
Benjamin Chrobot <benjamin.chrobot@alum.mit.edu>
2022-08-28 07:11:31 -0400

    refactor: pull up gql query

--- src/containers/AdminPeople/PeopleTable.tsx
+++ src/containers/AdminPeople/PeopleTable.tsx
@@ -1,10 +1,10 @@
 /* eslint-disable react/no-unstable-nested-components */
-import { gql } from "@apollo/client";
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
 import TableHead from "@material-ui/core/TableHead";
 import PeopleIcon from "@material-ui/icons/People";
+import { GetOrganizationPeopleDocument } from "@spoke/spoke-codegen";
 import React from "react";
 
 import {
@@ -18,42 +18,6 @@ import { AdminPeopleContext, PeopleRowEventHandlers } from "./context";
 import PeopleRow from "./PeopleRow";
 
 const PAGE_SIZE = 20;
-
-const query = gql`
-  query getPeople(
-    $organizationId: String!
-    $after: Cursor
-    $first: Int
-    $filter: MembershipFilter
-  ) {
-    organization(id: $organizationId) {
-      id
-      peopleCount
-      memberships(first: $first, after: $after, filter: $filter) {
-        edges {
-          node {
-            id
-            user {
-              id
-              firstName
-              lastName
-              displayName
-              email
-              isSuspended
-            }
-            role
-            requestAutoApprove
-          }
-        }
-        pageInfo {
-          endCursor
-          hasNextPage
-          totalCount
-        }
-      }
-    }
-  }
-`;
 
 interface PeopleData {
   organization: {
@@ -101,7 +65,7 @@ const PeopleTable: React.FC<PeopleTableProps> = ({
       </TableHead>
       <TableBody>
         <InfiniteRelayList<PeopleData, OrganizationMembership, PeopleVariables>
-          query={query}
+          query={GetOrganizationPeopleDocument}
           queryVars={{
             organizationId: context.organization.id,
             first: PAGE_SIZE,
