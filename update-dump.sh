eb15d5980d7c354e60d6748ffc9b5074c6127230
Benjamin Chrobot <benjamin.chrobot@alum.mit.edu>
2022-09-04 19:06:42 -0400

    fix: add support for apple m1 chips

--- update-dump.sh
+++ update-dump.sh
@@ -7,6 +7,7 @@ echo "Spinning up Postgres Docker container..."
 
 # Start the container in the background
 CONTAINER_ID=$(docker container run -d  \
+  --platform linux/amd64 \
   -p $PG_HOST_PORT:5432  \
   -e ALLOW_NOSSL=true \
   -e POSTGRES_DB=postgres \
