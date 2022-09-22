b7201be2933bccb47d362e80017fe15f64a69dbc
Hiemanshu Sharma <hiemanshu@gmail.com>
2022-09-22 17:52:24 +0530

    chore: list chore in changelog and release docs

--- .versionrc.js
+++ .versionrc.js
@@ -1,3 +1,13 @@
 module.exports = {
-  sign: true
+  sign: true,
+  types: [
+    { type: "feat", section: "Features" },
+    { type: "fix", section: "Bug Fixes" },
+    { type: "chore", hidden: false, section: "Backend Changes" },
+    { type: "docs", hidden: true },
+    { type: "style", hidden: true },
+    { type: "refactor", hidden: true },
+    { type: "perf", hidden: true },
+    { type: "test", hidden: true }
+  ]
 };
