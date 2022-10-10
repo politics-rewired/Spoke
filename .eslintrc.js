fd78d2810d3aac3af06dc8d8f0685c6eb1381fb5
Benjamin Chrobot <benjamin.chrobot@alum.mit.edu>
2022-10-10 07:04:11 -0400

    chore(mui): upgrade badges

--- .eslintrc.js
+++ .eslintrc.js
@@ -100,6 +100,7 @@ module.exports = {
           {
             name: "material-ui",
             importNames: [
+              "Badge",
               "Chip",
               "List",
               "ListItem",
@@ -131,6 +132,7 @@ module.exports = {
           },
           {
             group: [
+              "material-ui/Badge",
               "material-ui/Chip",
               "material-ui/List",
               "material-ui/ListItem",
