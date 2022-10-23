a866c11cd80559e9370456e390320a39b926bdd4
Benjamin Chrobot <benjamin.chrobot@alum.mit.edu>
2022-10-23 09:49:14 -0400

    test: pull up common code

--- src/server/api/lib/autosend-initials.spec.ts
+++ src/server/api/lib/autosend-initials.spec.ts
@@ -3,6 +3,7 @@ import {
   createCampaignContact
 } from "__test__/testbed-preparation/core";
 import type { campaign as CampaignRecord } from "@spoke/spoke-codegen";
+import type { PoolClient } from "pg";
 import { Pool } from "pg";
 import { UserRoleType } from "src/api/organization-membership";
 import supertest from "supertest";
@@ -12,6 +13,56 @@ import { config } from "../../../config";
 import { createApp } from "../../app";
 import { withClient } from "../../utils";
 import { MessageStatusType } from "../types";
+
+const createTestBed = async (
+  client: PoolClient,
+  agent: supertest.SuperAgentTest,
+  opts?: { mps?: number }
+) => {
+  const { organization, user, cookies } = await createOrgAndSession(client, {
+    agent,
+    role: UserRoleType.OWNER,
+    ...(opts?.mps !== undefined
+      ? { orgOptions: { autosending_mps: opts.mps } }
+      : {})
+  });
+  const campaign = await createCampaign(client, {
+    organizationId: organization.id,
+    isStarted: true
+  });
+  await Promise.all(
+    [...Array(5)].map(() =>
+      createCampaignContact(client, {
+        campaignId: campaign.id,
+        messageStatus: MessageStatusType.NeedsMessage
+      })
+    )
+  );
+  return { organization, user, cookies, campaign };
+};
+
+const queueCampaign = async (
+  agent: supertest.SuperAgentTest,
+  cookies: Record<string, string>,
+  campaignId: number
+) =>
+  agent
+    .post(`/graphql`)
+    .set(cookies)
+    .send({
+      operationName: "StartAutosending",
+      variables: {
+        campaignId: `${campaignId}`
+      },
+      query: `
+        mutation StartAutosending($campaignId: String!) {
+          startAutosending(campaignId: $campaignId) {
+            id
+            autosendStatus
+          }
+        }
+      `
+    });
 
 describe("autosend initials mutations", () => {
   let pool: Pool;
@@ -29,46 +80,10 @@ describe("autosend initials mutations", () => {
 
   it("creates queue-autosend-organization-initials job on startAutsending", async () => {
     const testbed = await withClient(pool, async (client) => {
-      const { organization, user, cookies } = await createOrgAndSession(
-        client,
-        {
-          agent,
-          role: UserRoleType.OWNER,
-          orgOptions: { autosending_mps: 30 }
-        }
-      );
-      const campaign = await createCampaign(client, {
-        organizationId: organization.id,
-        isStarted: true
-      });
-      await Promise.all(
-        [...Array(5)].map(() =>
-          createCampaignContact(client, {
-            campaignId: campaign.id,
-            messageStatus: MessageStatusType.NeedsMessage
-          })
-        )
-      );
-      return { organization, user, cookies, campaign };
+      return createTestBed(client, agent, { mps: 30 });
     });
 
-    await agent
-      .post(`/graphql`)
-      .set(testbed.cookies)
-      .send({
-        operationName: "StartAutosending",
-        variables: {
-          campaignId: `${testbed.campaign.id}`
-        },
-        query: `
-          mutation StartAutosending($campaignId: String!) {
-            startAutosending(campaignId: $campaignId) {
-              id
-              autosendStatus
-            }
-          }
-        `
-      });
+    await queueCampaign(agent, testbed.cookies, testbed.campaign.id);
 
     const {
       rows: [campaign]
