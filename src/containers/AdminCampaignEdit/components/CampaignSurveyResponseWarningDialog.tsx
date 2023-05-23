69443d50e3c355d10532a3bf2ab4dd6604e25b01
henryk1229 <henryk1229@gmail.com>
2023-05-23 13:32:29 -0400

    feat(survey-response-warning-dialog): initial component

---
+++ src/containers/AdminCampaignEdit/components/CampaignSurveyResponseWarningDialog.tsx
@@ -0,0 +1,71 @@
+import Button from "@material-ui/core/Button";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import React from "react";
+
+interface CampaignSurveyResponseWarningDialogProps {
+  open: boolean;
+  campaignId: string;
+  confirmWarningAndStartCampaign: () => void;
+  setShowConfirmDialog: (bool: boolean) => void;
+}
+
+const CampaignSurveyResponseWarningDialog = ({
+  open,
+  campaignId,
+  confirmWarningAndStartCampaign,
+  setShowConfirmDialog
+}: CampaignSurveyResponseWarningDialogProps) => {
+  return (
+    <Dialog open={open}>
+      <DialogTitle>
+        {`Heads up! Campaign ${campaignId} currently has no survey responses. Do you want to start this campaign anyway?`}
+      </DialogTitle>
+      <DialogContent>
+        Without survey responses, texters can only use{" "}
+        <a
+          href="https://docs.spokerewired.com/article/67-tags"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Tags
+        </a>{" "}
+        to log data on this campaign. Tags do not prepopulate scripts for
+        texters and are not scoped to a single campaign. Canned responses
+        prepopulate scripts but log no data at all. (
+        <a
+          href="https://docs.spokerewired.com/article/125-survey-responses-vs-canned-responses"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Read more
+        </a>
+        ) For the best experience for you and your texters, we recommend adding
+        survey responses before starting this campaign!
+      </DialogContent>
+      <DialogActions>
+        {[
+          <Button
+            key="save"
+            variant="contained"
+            onClick={confirmWarningAndStartCampaign}
+          >
+            Start this campaign
+          </Button>,
+          <Button
+            key="close"
+            variant="contained"
+            color="primary"
+            onClick={() => setShowConfirmDialog(false)}
+          >
+            Cancel
+          </Button>
+        ]}
+      </DialogActions>
+    </Dialog>
+  );
+};
+
+export default CampaignSurveyResponseWarningDialog;
