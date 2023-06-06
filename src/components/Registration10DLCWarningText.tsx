99157f3b696a7c2ef3679ac13fb5f1ecef6a32bc
Aashish John <aashishjohn25@gmail.com>
2023-06-06 19:02:50 -0400

    chore: update with csi feedback

--- src/components/Registration10DLCWarningText.tsx
+++ src/components/Registration10DLCWarningText.tsx
@@ -7,7 +7,7 @@ const Registration10DLCWarningText: React.FC = () => (
       by July 1st, 2023 for the best messaging experience! You may continue
       sending without registering for 10DLC, but you will incur increased
       charges for messaging, and may see lower message deliverability starting
-      on July 1st!
+      on July 1st.
     </p>
     <p>
       To learn more about this change, please see our{" "}
