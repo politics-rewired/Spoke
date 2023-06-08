d484f99a43cb00dd7c22f63e24002d5a27e4bcd3
Aashish John <aashishjohn25@gmail.com>
2023-06-08 18:25:50 -0400

    chore(10dlc): remove july date from notification

--- src/components/Registration10DLCWarningText.tsx
+++ src/components/Registration10DLCWarningText.tsx
@@ -4,10 +4,9 @@ const Registration10DLCWarningText: React.FC = () => (
   <>
     <p>
       Provide details required for us to register a 10DLC brand on your behalf
-      by July 1st, 2023 for the best messaging experience! You may continue
-      sending without registering for 10DLC, but you will incur increased
-      charges for messaging, and may see lower message deliverability starting
-      on July 1st.
+      for the best messaging experience! You may continue sending without 
+      registering for 10DLC, but you will incur increased charges for 
+      messaging, and may see lower message deliverability.
     </p>
     <p>
       To learn more about this change, please see our{" "}
