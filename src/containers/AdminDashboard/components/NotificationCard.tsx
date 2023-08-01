ec30c6b0cde2a86f48784698a4a0f1a54a7300a0
Aashish John <aashishjohn25@gmail.com>
2023-08-01 19:10:34 -0400

    refactor: use codegen

--- src/containers/AdminDashboard/components/NotificationCard.tsx
+++ src/containers/AdminDashboard/components/NotificationCard.tsx
@@ -1,8 +1,7 @@
-import { gql } from "@apollo/client";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import Divider from "@material-ui/core/Divider";
-import type { NoticePage } from "@spoke/spoke-codegen";
+import { useGetOrganizationNotificationsQuery } from "@spoke/spoke-codegen";
 import React from "react";
 
 import {
@@ -12,35 +11,34 @@ import {
   isRegister10DlcBrandNotice,
   isRegister10DlcCampaignNotice
 } from "../../../api/notice";
-import type { QueryMap } from "../../../network/types";
-import { withOperations } from "../../hoc/with-operations";
 import Pending10DlcCampaignNoticeCard from "./Pending10DlcCampaignNoticeCard";
 import PricingNoticeCard from "./PricingNoticeCard";
 import Register10DlcNoticeCard from "./Register10DlcNoticeCard";
 
-interface InnerProps {
+interface NotificationCardProps {
   organizationId: string;
-  loading: boolean;
-  data: {
-    error?: any;
-    notices: NoticePage;
-  };
 }
 
-export const NotificationCard: React.FC<InnerProps> = (props) => {
-  if (props.loading) {
-    return null;
-  }
-  if (props.data.error || !props.data.notices) {
+export const NotificationCard: React.FC<NotificationCardProps> = ({
+  organizationId
+}) => {
+  const { data, loading, error } = useGetOrganizationNotificationsQuery({
+    variables: { organizationId }
+  });
+
+  if (loading) return null;
+
+  if (error || !data?.notices) {
     return (
       <Card style={{ marginBottom: "2em" }}>
         <CardContent>There was an error fetching notifications.</CardContent>
       </Card>
     );
   }
+
   return (
     <div>
-      {props.data.notices.edges.map(({ node }) => {
+      {data?.notices.edges.map(({ node }) => {
         if (window.SHOW_10DLC_REGISTRATION_NOTICES) {
           if (
             isRegister10DlcBrandNotice(node) ||
@@ -54,52 +52,9 @@ export const NotificationCard: React.FC<InnerProps> = (props) => {
         }
         return null;
       })}
-      {props.data.notices.pageInfo.totalCount > 0 && <Divider />}
+      {data?.notices.pageInfo.totalCount > 0 && <Divider />}
     </div>
   );
 };
 
-const queries: QueryMap<InnerProps> = {
-  data: {
-    query: gql`
-      query getOrganizationNotifications($organizationId: String!) {
-        notices(organizationId: $organizationId) {
-          pageInfo {
-            totalCount
-          }
-          edges {
-            node {
-              ... on Register10DlcBrandNotice {
-                id
-                tcrRegistrationUrl
-              }
-              ... on Register10DlcCampaignNotice {
-                id
-                tcrRegistrationUrl
-              }
-              ... on Pending10DlcCampaignNotice {
-                id
-              }
-              ... on Pricing10DlcNotice {
-                id
-              }
-              ... on PricingTollFreeNotice {
-                id
-              }
-            }
-          }
-        }
-      }
-    `,
-    options: (ownProps) => ({
-      variables: {
-        organizationId: ownProps.organizationId
-      },
-      fetchPolicy: "network-only"
-    })
-  }
-};
-
-export default withOperations({
-  queries
-})(NotificationCard);
+export default NotificationCard;
