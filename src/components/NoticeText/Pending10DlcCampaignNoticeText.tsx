84e29beb1e2127ff48be35868d569c4d417019b8
Aashish John <aashishjohn25@gmail.com>
2023-08-01 21:26:34 -0400

    chore: specify external vetting
    Co-authored-by: Benjamin Chrobot <benjamin.chrobot@alum.mit.edu>

--- src/components/NoticeText/Pending10DlcCampaignNoticeText.tsx
+++ src/components/NoticeText/Pending10DlcCampaignNoticeText.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Pending10DlcCampaignNoticeText: React.FC = () => (
   <p>
-    Your 10DLC campaign has been submitted for vetting. Please allow up to one
+    Your 10DLC campaign has been submitted for external vetting. Please allow up to one
     week for this process to be completed.
     <br /> <br />
     If your campaign remains in this status for longer than a week, please reach
