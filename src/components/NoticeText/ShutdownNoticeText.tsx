9e1bc5d2821416260e5550f80829cc208195b813
Aashish John <aashishjohn25@gmail.com>
2023-11-13 19:11:48 -0500

    fix: better styling

--- src/components/NoticeText/ShutdownNoticeText.tsx
+++ src/components/NoticeText/ShutdownNoticeText.tsx
@@ -4,10 +4,17 @@ const ShutdownNoticeText: React.FC = () => (
   <>
     <p>
       Politics Rewired is winding down operations. Text messages will no longer
-      be deliverable after <strong> November 15</strong>, 2023 and your instance
-      will no longer be available after <strong> November 22</strong>, 2023.
-      Please find more details in our transition document here:
-      politicsrewired.com
+      be deliverable after <strong> November 15, 2023 </strong> and your
+      instance will no longer be available after{" "}
+      <strong> November 22, 2023</strong>. Please find more details in our
+      transition document here:{" "}
+      <a
+        href="https://www.politicsrewired.com"
+        target="_blank"
+        rel="noreferrer"
+      >
+        politicsrewired.com
+      </a>
     </p>
     <p>
       Thank you for choosing us for your texting outreach, and we wish you the
